0fca350263da36b86eb8636a352477ad4676051c
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-07-16 13:40:25 +0200

    Provide example movie API
    This is just for getting familiar with all GraphQL tooling/libraries
    and will be completely removed later.

--- backend/src/api/query.rs
+++ backend/src/api/query.rs
@@ -8,4 +8,30 @@ impl Query {
     fn apiVersion() -> &str {
         "0.0"
     }
+
+    fn movies() -> &[Movie] {
+        MOVIES
+    }
+
+    fn movie(id: i32) -> Option<Movie> {
+        MOVIES.get(id as usize).copied()
+    }
+
+    fn oldestMovie() -> Movie {
+        *MOVIES.iter().min_by_key(|m| m.year).unwrap()
+    }
 }
+
+#[derive(Debug, Clone, Copy, juniper::GraphQLObject)]
+struct Movie {
+    id: i32,
+    name: &'static str,
+    year: i32,
+}
+
+const MOVIES: &[Movie] = &[
+    Movie { id: 0, name: "The Prestige", year: 2006 },
+    Movie { id: 1, name: "Ghost in the Shell", year: 1995 },
+    Movie { id: 2, name: "Atonement", year: 2007 },
+    Movie { id: 3, name: "I Origins", year: 2014 },
+];
