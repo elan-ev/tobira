0879e1f0743f7750ed37f86c6f7649b772a57f99
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-07-16 18:15:41 +0200

    Add Apollo implementation of example
    It works, but one thing is still problematic: the `gql` JS template
    does not generate TS typings. There are a few way to pre-process gql,
    but none of those generates types either I think. We can manually
    specify types, but this feels like duplication, as the schema already
    defines all the types.
    
    Maybe specifying types in the frontend again is good to make sure
    frontend and backend agree on the types. But in that case we would like
    to have a test to check that both agree on the types. So in either way,
    we need some way to generate types from the schema and `gql`
    invocations.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,31 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { Movie, Movies } from './Movie';
 
 export const App: React.FC = () => {
-    return <h1>Yes Hello</h1>;
+  const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route path="/movie/:id">
+            <Movie />
+          </Route>
+          <Route path="/">
+            <Movies />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
+  );
 };
