9a0c817e48fa55b38189d8730843ec75d908be6e
Julian Kniephoff <kniephoff@elan-ev.de>
2020-08-13 08:13:06 +0200

    Show the oldest movie on the movie list

--- frontend/src/Movie.tsx
+++ frontend/src/Movie.tsx
@@ -1,29 +1,65 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 
-import { graphql, useLazyLoadQuery } from 'react-relay/hooks';
-import { MovieQuery } from './__generated__/MovieQuery.graphql';
+import { graphql, useFragment, useLazyLoadQuery } from 'react-relay/hooks';
 import { MoviesQuery } from './__generated__/MoviesQuery.graphql';
+import { MovieList_movies$key } from './__generated__/MovieList_movies.graphql';
+import { MovieListEntry_movie$key } from './__generated__/MovieListEntry_movie.graphql';
+import { MovieQuery } from './__generated__/MovieQuery.graphql';
 
 
 export const Movies: React.FC = () => {
-  const { movies } = useLazyLoadQuery<MoviesQuery>(graphql`
+  const data = useLazyLoadQuery<MoviesQuery>(graphql`
     query MoviesQuery {
-      movies {
-        id
-        name
-        year
+      ... MovieList_movies
+      oldestMovie {
+        ... MovieListEntry_movie
       }
     }
   `, {});
+  return <>
+    <MovieList data={data} />
+
+    <h1>Oldest movie</h1>
+    <MovieListEntry movie={data.oldestMovie} />
+  </>;
+};
+
+interface MovieListProps {
+  data: MovieList_movies$key
+}
+const MovieList: React.FC<MovieListProps> = ({ data }: MovieListProps) => {
+  const { movies } = useFragment(graphql`
+    fragment MovieList_movies on Query {
+      movies {
+        id
+        ... MovieListEntry_movie
+      }
+    }
+  `, data);
 
   return <ul>
-    {movies.map(({ id, name, year }) => (
-      <li key={id}>
-        <Link to={`/movie/${id}`}>{name} ({year})</Link>
+    {movies.map(movie => (
+      <li key={movie.id}>
+        <MovieListEntry movie={movie} />
       </li>
     ))}
   </ul>
+};
+
+interface MovieListEntryProps {
+  movie: MovieListEntry_movie$key
+}
+const MovieListEntry: React.FC<MovieListEntryProps> = ({ movie }: MovieListEntryProps) => {
+  const { id, name, year } = useFragment(graphql`
+    fragment MovieListEntry_movie on Movie {
+      id
+      name
+      year
+    }
+  `, movie);
+
+  return <Link to={`/movie/${id}`}>{name} ({year})</Link>
 };
 
 export const Movie: React.FC = () => {
