38dbb1dd28a8884b02130b6edefd069154a15921
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-09-17 18:13:31 +0200

    Add `static` folder for static files that are copied to `build/static`
    Copying is done by webpack. The folder already contains two versions
    of the placeholder logo.

--- frontend/constants.js
+++ frontend/constants.js
@@ -3,4 +3,5 @@ const path = require("path");
 module.exports = {
     APP_PATH: path.join(__dirname, "src"),
     OUT_PATH: path.join(__dirname, "build"),
+    STATIC_PATH: path.join(__dirname, "static"),
 };
