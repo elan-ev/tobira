7d8b9afd6fb7b0b404ea0aec3b5a33eb671e07ed
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-09-18 14:14:15 +0200

    Use "Open Sans" typeface as default typeface for the whole page
    There are several reasons in favor of Open Sans:
    
    - Opencast uses Open Sans in most of its software, on its website and
      in many other places. It's basically part of the "cooperate identity".
    - It is a fairly neutral, but clean and modern looking typeface.
    - Many users actually already have Open Sans installed locally.
    - If I were to look for another typeface for use to use as default, I
      would be gone for days before emerging frustratedly screaming "there
      are too many good fonts!"

--- frontend/src/GlobalStyle.tsx
+++ frontend/src/GlobalStyle.tsx
@@ -2,15 +2,17 @@ import { css, jsx, Global } from "@emotion/core";
 import React from "react";
 
 
-export const GlobalStyle: React.FC = () => <Global styles={GLOBAL_STYLE} />;
+export const GlobalStyle: React.FC = () => <>
+    <Global styles={CSS_RESETS} />
+    <Global styles={GLOBAL_STYLE} />
+</>;
 
-const GLOBAL_STYLE = css({
-    // ===== CSS Resets ===================================================
-    // The following is a minimal set of CSS reset rules in order to get rid of
-    // browser dependent, inconsistent or unexpected behavior. Parts of this
-    // are taken from here: https://github.com/hankchizljaw/modern-css-reset
-    // Licensed as MIT, Andy Bell and other contributors
 
+// The following is a minimal set of CSS reset rules in order to get rid of
+// browser dependent, inconsistent or unexpected behavior. Parts of this
+// are taken from here: https://github.com/hankchizljaw/modern-css-reset
+// Licensed as MIT, Andy Bell and other contributors
+const CSS_RESETS = css({
     // Everything should have box-sizing border-box by default as it's more
     // intuitive and expected.
     "*, *::before, *::after": {
@@ -49,5 +51,13 @@ const GLOBAL_STYLE = css({
     // Some elements not inhereting fonts is a really confusing browser default.
     "input, button, textarea, select": {
         font: "inherit",
+    },
+});
+
+// This is just styling for Tobira that we want to apply globally.
+const GLOBAL_STYLE = css({
+    "body": {
+        fontFamily: "'Open Sans', sans-serif",
+        fontWeight: 400,
     },
 });
