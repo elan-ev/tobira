f43d856658f62f27ca873b5c85608b765f5bd233
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-09-24 17:37:52 +0200

    Fix minor lint error

--- frontend/src/page/NotFound.tsx
+++ frontend/src/page/NotFound.tsx
@@ -4,6 +4,6 @@ import React from "react";
 export const NotFound: React.FC = () => {
     return <>
         <h1>404 Page not found</h1>
-        <p>No idea how you got here, but it's wrong :(</p>
+        <p>No idea how you got here, but there is nothing here :(</p>
     </>;
 };
