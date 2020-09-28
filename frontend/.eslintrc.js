29eeafabb323efc2848c406fdc77f806af23bc84
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-09-28 16:08:09 +0200

    Disable two lint rules
    - react-in-jsx-scope: that's not actually required in our setup
    - no-explicit-any: sometimes `any` is unfortunately needed. Mentioning
      it explicitly should be fine.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -30,6 +30,8 @@ module.exports = {
         ],
         rules: {
             "react/prop-types": "off",
+            "react/react-in-jsx-scope": "off",
+            "@typescript-eslint/no-explicit-any": "off",
         },
         settings: {
             react: {
