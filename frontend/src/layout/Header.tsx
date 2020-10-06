0998a2be329c654a837026c8d232f540e36405d3
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-10-06 18:25:24 +0200

    Improve padding/margin of header and logo

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 
 const HEIGHT = 60;
@@ -14,7 +15,6 @@ export const Header: React.FC = () => (
         justifyContent: "space-between",
         backgroundColor: "white",
         borderBottom: "1px solid #bbb",
-        padding: "0 8px",
     }}>
         <Logo />
         <Search />
@@ -24,12 +24,12 @@ export const Header: React.FC = () => (
 
 const Logo: React.FC = () => (
     <Link to="/" css={{ height: "100%", flex: "0 0 auto" }}>
-        <picture css={{
-            height: "100%",
-            "& > *": { height: "100%", padding: "4px 0" },
-        }}>
+        <picture css={{ height: "100%" }}>
             <source media="(min-width: 450px)" srcSet="/assets/static/logo-large.svg" />
-            <img src="/assets/static/logo-small.svg" />
+            <img
+                css={{ height: "100%", padding: "4px 8px" }}
+                src="/assets/static/logo-small.svg"
+            />
         </picture>
     </Link>
 );
