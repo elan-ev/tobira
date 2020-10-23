44e706d4d67ecd96f1e92ca41cc34ae4d7af73af
Julian Kniephoff <kniephoff@elan-ev.de>
2020-10-23 16:33:12 +0200

    Add a path segment to the realm table

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -1,9 +1,10 @@
 create table realms (
     id bigint generated always as identity (start with 0 minvalue 0) primary key,
     parent bigint not null references realms on delete restrict,
-    name text not null
+    name text not null,
+    path text not null
 );
 -- To truncate this and restart the primary key counter:
 --truncate realms restart identity;
 
-insert into realms (name, parent) values ('root', 0);
+insert into realms (name, parent, path) values ('root', 0, '');
