b2e5e761db806e4b1d7f066aa3b563193597d664
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2020-12-11 13:28:38 +0100

    Add dummy content blocks to `fixtures.sql`

--- scripts/fixtures.sql
+++ scripts/fixtures.sql
@@ -9,6 +9,15 @@ begin
     values (name, 0, replace(lower(name), ' ', '-'))
     returning id into root;
 
+    insert into blocks (realm_id, type, index, title, text_content)
+        values (root, 'text', '0', 'Description', format(
+            'Hello to the department of %s! We are very proud of what we have achieved in '
+                || 'this department and there is a lot of interesting stuff around here. '
+                || 'Take a look at our swell videos. And do not forget to like, subscribe '
+                || 'and hit that bell icon!',
+            name
+        ));
+
     for y in 2020 .. 2021 loop
         insert into realms (name, parent, path_segment)
         values (y, root, y::text)
@@ -26,3 +35,9 @@ select department('Computer Science');
 select department('Physics');
 
 drop function department;
+
+insert into blocks (realm_id, type, index, text_content)
+    values (
+        0, 'text', 0,
+        'Welcome to Tobira! This database contains dummy data intended for development. Have fun!'
+    );
