0f4e73289c765818b77db906d72abe2d872db9dc
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-01-08 13:20:50 +0100

    Fix checkmark in language menu when language is inferred
    When no language was set, but is inferred, `i18n.language` returns
    something like "en-US", but our language keys don't include the region.
    With this, we use a different either hacky/smart way to determine the
    active language.

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -123,24 +123,40 @@ const LanguageList = () => {
             margin: 0,
             padding: 0,
         }}>
-            {Object.keys(languages).map(lng => (
-                <li
-                    onClick={() => i18n.changeLanguage(lng)}
-                    key={lng}
-                    css={{
-                        padding: "4px 8px 4px 12px",
-                        cursor: "pointer",
-                        "&:hover": {
-                            backgroundColor: "#ddd",
-                        },
-                    }}
-                >
-                    <div className="fa-fw" css={{ display: "inline-block", marginRight: 8 }}>
-                        {lng === i18n.language && <FontAwesomeIcon icon={faCheck} fixedWidth />}
-                    </div>
-                    {t("language-name", { lng })}
-                </li>
-            ))}
+            {Object.keys(languages).map(lng => {
+                // We check whether a language is active by simply translating
+                // with the automatically determined language and with the
+                // specific one. If the result is the same, that's the current
+                // language.
+                //
+                // I do realize that it could be that one language "foo" has
+                // only the string "language-name" translated and thus, the
+                // whole page would be shown with the fallback language (en),
+                // but the language menu would still show "foo" as active
+                // language. Or imagine the other way around: everything except
+                // "language-name" is translated. But both of these cases should
+                // be avoided anyway when creating a translation.
+                const isActive = t("language-name", { lng }) === t("language-name");
+
+                return (
+                    <li
+                        onClick={() => i18n.changeLanguage(lng)}
+                        key={lng}
+                        css={{
+                            padding: "6px 8px 6px 12px",
+                            cursor: "pointer",
+                            "&:hover": {
+                                backgroundColor: "#ddd",
+                            },
+                        }}
+                    >
+                        <div className="fa-fw" css={{ display: "inline-block", marginRight: 16 }}>
+                            {isActive && <FontAwesomeIcon icon={faCheck} fixedWidth />}
+                        </div>
+                        {t("language-name", { lng })}
+                    </li>
+                );
+            })}
         </ul>
     );
 };
