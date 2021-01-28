ed8542d31524c611e95243de98bf1f9c4687fe2e
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-01-28 17:41:40 +0100

    Add video list rendering (grid layout) with dummy data

--- frontend/src/ui/Blocks.tsx
+++ frontend/src/ui/Blocks.tsx
@@ -57,12 +57,77 @@ const TextBlock: React.FC<TextProps> = ({ title, content }) => (
     </Block>
 );
 
-const VideoListBlock: React.FC<{ title: string | null }> = ({ title }) => (
-    <Block>
-        <Title title={title} />
-        <i>not yet implemented ☹️</i>
-    </Block>
-);
+const VideoListBlock: React.FC<{ title: string | null }> = ({ title }) => {
+    const [THUMB_WIDTH, THUMB_HEIGHT] = [16, 9].map(x => x * 13);
+
+    return (
+        <Block>
+            <Title title={title} />
+            <div css={{
+                display: "flex",
+                flexWrap: "wrap",
+            }}>
+                {DUMMY_VIDEOS.map(v => {
+                    const url = `/v/${v.id}`;
+
+                    return (
+                        <div
+                            key={v.id}
+                            css={{
+                                margin: "12px 8px",
+                                width: THUMB_WIDTH,
+                                "& a": { color: "black", textDecoration: "none" },
+                            }}
+                        >
+                            <a href={url} css={{ position: "relative", display: "block" }}>
+                                <img
+                                    src={v.thumbnail}
+                                    width={THUMB_WIDTH}
+                                    height={THUMB_HEIGHT}
+                                    css={{ display: "block" }}
+                                />
+                                <div css={{
+                                    position: "absolute",
+                                    right: 6,
+                                    bottom: 6,
+                                    backgroundColor: "hsla(0, 0%, 0%, 0.75)",
+                                    border: "1px solid black",
+                                    borderRadius: 4,
+                                    padding: "0 4px",
+                                    color: "white",
+                                }}>
+                                    {formatLength(v.length)}
+                                </div>
+                            </a>
+
+                            <h3 css={{
+                                fontSize: 16,
+                            }}>
+                                <a href={url}>{v.title}</a>
+                            </h3>
+                        </div>
+                    );
+                })}
+            </div>
+        </Block>
+    );
+};
+
+const formatLength = (totalSeconds: number) => {
+    const seconds = totalSeconds % 60;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const hours = Math.floor(totalSeconds / (60 * 60));
+
+    const pad = (n: number) => (n < 10 ? `0${n}` : `${n}`);
+
+    if (hours > 0) {
+        return `${hours}:${pad(minutes)}:${pad(seconds)}`;
+    } else if (minutes > 0) {
+        return `${minutes}:${pad(seconds)}`;
+    } else {
+        return `0:${pad(seconds)}`;
+    }
+};
 
 const Title: React.FC<{ title: string | null }> = ({ title }) => (
     title === null ? null : <h2>{title}</h2>
@@ -90,3 +155,48 @@ function unwrap<K extends keyof BlockData>(block: BlockData, field: K): NonNulla
 
     return v;
 }
+
+const DUMMY_VIDEOS = [
+    {
+        id: 0,
+        title: "Programmieren in Rust: Einführung",
+        length: 5123,
+        thumbnail: "https://i.imgur.com/QtsTbCi.jpg",
+    },
+    {
+        id: 1,
+        title: "Programmieren in Rust: Module",
+        length: 5018,
+        thumbnail: "https://i.imgur.com/aoRSWgJ.jpg",
+    },
+    {
+        id: 2,
+        title: "The Physics behind a flying Bumblebee",
+        length: 47,
+        thumbnail: "https://i.imgur.com/TuS6Qxg.jpg",
+    },
+    {
+        id: 3,
+        title: "Programmieren in Rust: Performance & Effizienz",
+        length: 5329,
+        thumbnail: "https://i.imgur.com/4y40tkN.jpg",
+    },
+    {
+        id: 4,
+        title: "Spring (Open Blender Movie)",
+        length: 464,
+        thumbnail: "https://i.imgur.com/JhUYB7C.jpg",
+    },
+    {
+        id: 5,
+        title: "Cosmos Laundromat (Open Blender Movie)",
+        length: 730,
+        thumbnail: "https://i.imgur.com/qGOL4Jy.jpg",
+    },
+    {
+        id: 6,
+        title: "Programmieren in Rust: Stack & Heap",
+        length: 5399,
+        thumbnail: "https://i.imgur.com/XBDOO2R.jpg",
+    },
+];
