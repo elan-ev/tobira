c8acf1c8f94673e1059d67dfd68dc3f9d0f284f5
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-05-04 17:51:42 +0200

    Add main loop logic for harvesting (including error handling)
    The idea is:
    - When `has_more` is `true`, send the next request immediately.
    - When `has_more` is `false`, wait a specific time before sending the
      next request.
    - If an error occurs, log it, and wait a bit before trying again. The
      duration for which to wait increases exponentially until hitting a
      ceiling.

--- backend/server/src/sync/mod.rs
+++ backend/server/src/sync/mod.rs
@@ -1,12 +1,36 @@
-use hyper::StatusCode;
-use hyper14::http::uri::{Authority, Scheme, Uri};
-use hyper14::{Request, client::Client};
+use std::{
+    cmp::min,
+    time::Duration,
+};
+
+use chrono::{DateTime, NaiveDateTime, Utc};
+use hyper14::{
+    Request, Body,
+    body::Bytes,
+    client::{Client, HttpConnector},
+    http::{
+        response,
+        status::StatusCode,
+        uri::{Authority, Scheme, Uri},
+    }
+};
 use hyper_tls::HttpsConnector;
-use secrecy::ExposeSecret;
+use secrecy::{ExposeSecret, Secret};
 use serde::Deserialize;
 
 use tobira_util::prelude::*;
+use tokio_postgres::GenericClient;
 use crate::{config::Config, db};
+
+
+// TODO: make (some of) this stuff configurable.
+
+const HARVEST_LIMIT: u64 = 2;
+
+const INITIAL_BACKOFF: Duration = Duration::from_secs(2);
+const MAX_BACKOFF: Duration = Duration::from_secs(5 * 60);
+
+const POLL_PERIOD: Duration = Duration::from_secs(30);
 
 
 pub(crate) async fn run(config: &Config) -> Result<()> {
@@ -19,74 +43,189 @@ pub(crate) async fn run(config: &Config) -> Result<()> {
     db::migrate(&mut *db.get().await?).await
         .context("failed to check/run DB migrations")?;
 
-    harvest(config).await?;
+    let db_connection = db.get().await?;
+    harvest(config, &**db_connection).await?;
 
     Ok(())
 }
 
 /// Continuiously contacts the harvesting API and writes new data into our
 /// database.
-async fn harvest(config: &Config) -> Result<()> {
-    // Prepare HTTP client
-    let client = Client::builder().build::<_, hyper14::Body>(HttpsConnector::new());
+async fn harvest(config: &Config, db: &impl GenericClient) -> Result<()> {
+    // Some duration to wait before the next attempt. Is only set to non-zero in
+    // case of an error.
+    // TODO: replace by `ZERO` constant in two days.
+    let mut backoff = Duration::from_secs(0);
 
-    // Prepare URL
-    let scheme = if config.opencast.use_insecure_connection {
-        Scheme::HTTP
-    } else {
-        Scheme::HTTPS
-    };
-    let authority = config.opencast.host.parse::<Authority>().expect("bug: invalid config");
-    let path_query = |since: Timestamp, limit| {
-        format!("/tobira/harvest?since={}&limit={}", since.0, limit)
-    };
+    /// Helper macro to call in case of not being able to get a proper response
+    /// from Opencast. Forwards all arguments to `error!`, increases `backoff`
+    /// and sleeps for the backoff period.
+    macro_rules! request_failed {
+        ($($t:tt)*) => {{
+            error!($($t)*);
 
-    // Prepare authentication
-    let auth_header = {
+            // We increase the backoff duration exponentially until we hit the
+            // defined maximum.
+            // TODO: replace by `ZERO` constant in two days.
+            backoff = if backoff == Duration::from_secs(0) {
+                INITIAL_BACKOFF
+            } else {
+                min(MAX_BACKOFF, 2 * backoff)
+            };
+            info!("Waiting {:.0?} due to error before trying again", backoff);
+            tokio::time::sleep(backoff).await;
+
+            continue;
+        }};
+    }
+
+    let client = HarvestClient::new(config);
+
+    loop {
+        let sync_status = SyncStatus::fetch(db).await
+            .context("failed to fetch sync status from DB")?;
+
+
+        // Send request to API and deserialize data.
+        let (response, body) = match client.send(sync_status.harvested_until, HARVEST_LIMIT).await {
+            Ok(v) => v,
+            Err(e) => request_failed!("Harvest request failed: {:?}", e),
+        };
+
+        trace!("HTTP response: {:#?}", response);
+        if response.status != StatusCode::OK {
+            request_failed!("Harvest API returned unexepcted HTTP code {}", response.status);
+        }
+
+        let harvest_data = match serde_json::from_slice::<HarvestResponse>(&body) {
+            Ok(v) => v,
+            Err(e) => request_failed!("Failed to deserialize response from harvesting API: {}", e),
+        };
+
+
+        // Write received data into the database, updating the sync status if
+        // everything worked out alright.
+        store_in_db(&harvest_data, db).await?;
+        SyncStatus::update_harvested_until(harvest_data.includes_items_until, db).await?;
+        if !harvest_data.has_more {
+            debug!(
+                "Harvested all available data. Waiting {:?} before starting next harvest",
+                POLL_PERIOD,
+            );
+
+            tokio::time::sleep(POLL_PERIOD).await;
+        }
+    }
+}
+
+async fn store_in_db(data: &HarvestResponse, _db: &impl GenericClient) -> Result<()> {
+    // TODO
+    println!("{:#?}", data);
+
+    Ok(())
+}
+
+/// Used to send request to the harvesting API.
+struct HarvestClient {
+    http_client: Client<HttpsConnector<HttpConnector>, Body>,
+    scheme: Scheme,
+    authority: Authority,
+    auth_header: Secret<String>,
+}
+
+impl HarvestClient {
+    const API_PATH: &'static str = "/tobira/harvest";
+
+    fn new(config: &Config) -> Self {
+        // Prepare HTTP client
+        let http_client = Client::builder().build(HttpsConnector::new());
+
+        // Prepare URL
+        let scheme = if config.opencast.use_insecure_connection {
+            Scheme::HTTP
+        } else {
+            Scheme::HTTPS
+        };
+        let authority = config.opencast.host.parse::<Authority>().expect("bug: invalid config");
+
+        // Prepare authentication
         let credentials = format!(
             "{}:{}",
             config.opencast.sync_user,
             config.opencast.sync_password.expose_secret(),
         );
-        format!("Basic {}", base64::encode(credentials))
-    };
+        let auth_header = format!("Basic {}", base64::encode(credentials));
 
-    loop {
+        Self {
+            http_client,
+            scheme,
+            authority,
+            auth_header: Secret::new(auth_header),
+        }
+    }
+
+    /// Sends a request to the harvesting API. Returns the response "meta data"
+    /// and the downloaded response body.
+    async fn send(
+        &self,
+        since: DateTime<Utc>,
+        limit: u64,
+    ) -> Result<(response::Parts, Bytes)> {
+        let pq = format!(
+            "{}?since={}&limit={}",
+            Self::API_PATH,
+            since.timestamp_millis(),
+            limit,
+        );
+
         let uri = Uri::builder()
-            .scheme(scheme)
-            .authority(authority)
-            .path_and_query(&*path_query(Timestamp(0), 5))
+            .scheme(self.scheme.clone())
+            .authority(self.authority.clone())
+            .path_and_query(&*pq)
             .build()
             .expect("bug: failed build URI");
 
         let req = Request::builder()
             .uri(&uri)
-            .header("Authorization", auth_header)
-            .body(hyper14::Body::empty())
+            .header("Authorization", self.auth_header.expose_secret())
+            .body(Body::empty())
             .expect("bug: failed to build request");
 
-        debug!("Sending harvest request: GET {}", uri);
-        let res = client.request(req).await?;
+        debug!("Sending harvest request (since = {:?}): GET {}", since, uri);
+        let response = self.http_client.request(req).await
+            .with_context(|| format!("failed to GET {}", uri))?;
 
-        trace!("HTTP response: {:#?}", res);
-        if res.status() != StatusCode::OK {
-            todo!("non ok http status");
-        }
+        let (parts, body) = response.into_parts();
+        let body = hyper14::body::to_bytes(body).await
+            .with_context(|| format!("failed to download body from {}", uri))?;
+        Ok((parts, body))
+    }
+}
 
-        let body = hyper14::body::to_bytes(res).await?;
-        match serde_json::from_slice::<HarvestResponse>(&body) {
-            Err(e) => {
-                todo!("Failed to deserialize response: {}", e);
-            }
-            Ok(response) => {
-                println!("{:#?}", response);
-            }
-        }
+struct SyncStatus {
+    harvested_until: DateTime<Utc>,
+}
 
-        break;
+impl SyncStatus {
+    async fn fetch(db: &impl GenericClient) -> Result<Self> {
+        let row = db.query_one("select harvested_until from sync_status", &[]).await?;
+
+        Ok(Self {
+            harvested_until: DateTime::from_utc(row.get::<_, NaiveDateTime>(0), Utc),
+        })
     }
 
-    Ok(())
+    async fn update_harvested_until(
+        new_value: DateTime<Utc>,
+        db: &impl GenericClient,
+    ) -> Result<()> {
+        db.execute(
+            "update sync_status set harvested_until = $1",
+            &[&new_value.naive_utc()],
+        ).await?;
+
+        Ok(())
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -118,6 +257,3 @@ enum HarvestItem {
         updated: DateTime<Utc>,
     },
 }
-
-#[derive(Debug, Deserialize)]
-struct Timestamp(u64);
