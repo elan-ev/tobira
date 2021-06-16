e3de763db740853771cb484392dc787c3c13013a
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-06-16 15:44:34 +0200

    Apply <main> margin on wide screens to header as well
    Now that the border between header and <main> is gone, it looks weird
    if the header has a different width.

--- frontend/src/layout/Root.tsx
+++ frontend/src/layout/Root.tsx
@@ -5,7 +5,12 @@ import { Header } from "./Header";
 export const MAIN_PADDING = 16;
 
 export const Root: React.FC = ({ children }) => (
-    <div>
+    <div css={{
+        // This funky expressions just means: above a screen width of 1100px,
+        // the extra space will be 10% margin left and right. This is the middle
+        // ground between filling the full screen and having a fixed max width.
+        margin: "0 calc(max(0px, 100% - 1100px) * 0.1)",
+    }}>
         <Header />
         <main css={{ padding: MAIN_PADDING }}>{children}</main>
     </div>
