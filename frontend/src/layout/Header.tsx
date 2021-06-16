63045c26a0987b0bcd24b71f11d2d7274f874a94
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-06-16 19:44:22 +0200

    Remove old menu code and language icon
    The old menu is likely to be replaced fully by the new burger. I liked
    the language icon and think there is some use in making the language
    switcher quickly and easily available. However, others convinced me
    that it's mostly useless. The language is already guessed from the
    browser (and that's rarely incorrect; and if it it it's the user's
    fault). And Tobira will not support content in multiple languages for
    quite some time, so the language switcher is not really that useful
    compared to, for example, Opencast Studio. The latter application has
    lots more "application text" and a lot less "content text".
    
    Of course, the ability to switch the language will be added back at
    some point.

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -1,19 +1,13 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCheck, faInfoCircle, faCog, faMoon } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
 import { useTranslation } from "react-i18next";
-import LanguageIcon from "ionicons/dist/svg/language.svg";
 import MenuIcon from "ionicons/dist/svg/menu.svg";
 import MenuCloseIcon from "ionicons/dist/svg/close.svg";
 
-import { languages } from "../i18n";
-import { match } from "../util";
 import CONFIG from "../config";
 import { Link } from "../router";
 
 
 export const HEIGHT = 60;
-const HEADER_BORDER_WIDTH = 1;
 
 
 type Props = {
@@ -83,61 +77,14 @@ type ActionButtonProps = {
 const ActionIcons: React.FC<ActionButtonProps> = ({ setBurgerVisible, burgerVisible }) => {
     const { t } = useTranslation();
 
-    type MenuState = "closed" | "language" | "menu";
-    const [menuState, setMenuState] = useState<MenuState>("closed");
-    const toggleMenu = (state: MenuState) => {
-        setMenuState(menuState === state ? "closed" : state);
-    };
-
     return (
         <div css={{ display: "flex", height: "100%", position: "relative" }}>
-            {!burgerVisible && <ActionIcon
-                title={`${t("language")}: ${t("language-name")}`}
-                onClick={() => toggleMenu("language")}
-                isActive={menuState === "language"}
-            >
-                <LanguageIcon />
-            </ActionIcon>}
-
             <ActionIcon
                 title={t("main-menu.label")}
                 onClick={() => setBurgerVisible(!burgerVisible)}
-                isActive={menuState === "menu"}
             >
                 {burgerVisible ? <MenuCloseIcon /> : <MenuIcon />}
             </ActionIcon>
-
-            <div css={{
-                position: "absolute",
-                ...menuState === "closed" && { display: "none" },
-                top: `calc(100% + ${HEADER_BORDER_WIDTH}px)`,
-                right: 0,
-                zIndex: 20,
-                minWidth: 180,
-                border: "1px solid #bbb",
-                borderTop: "none",
-                borderRight: "none",
-                maxWidth: "100vw",
-                "&::after": {
-                    content: "''",
-                    position: "absolute",
-                    top: 10,
-                    bottom: -2,
-                    left: 0,
-                    right: 0,
-                    zIndex: -1,
-                    backgroundColor: "#0000001a",
-                    boxShadow: "0 0 7px 3px #0000001a",
-                },
-            }}>
-                <div css={{ backgroundColor: "white" }}>
-                    {match(menuState, {
-                        "closed": () => null,
-                        "language": () => <LanguageList />,
-                        "menu": () => <MainMenu closeMenu={() => setMenuState("closed")} />,
-                    })}
-                </div>
-            </div>
         </div>
     );
 };
@@ -145,44 +92,17 @@ const ActionIcons: React.FC<ActionButtonProps> = ({ setBurgerVisible, burgerVisi
 type ActionIconProps = {
     onClick: () => void;
     title: string;
-    isActive: boolean;
 };
 
-/**
- * A single icon/button on the right of the header. There is some trickery
- * involved to make this arrow/triangle indicator when a specific icon is
- * active.
- */
-const ActionIcon: React.FC<ActionIconProps> = ({ title, onClick, isActive, children }) => {
+/** A single button with icon in the header. */
+const ActionIcon: React.FC<ActionIconProps> = ({ title, onClick, children }) => {
     const iconSize = 28;
-    const arrowSize = 14;
 
     return (
         <div css={{
             height: "100%",
-            position: "relative",
             display: "flex",
             alignItems: "center",
-            ...isActive && {
-                "&::after": {
-                    content: "''",
-                    display: "block" as const,
-                    position: "absolute" as const,
-                    bottom: -arrowSize / 2,
-                    backgroundColor: "white",
-                    zIndex: 10,
-                    width: arrowSize,
-                    height: arrowSize,
-                    borderTop: "1px solid #bbb",
-                    borderLeft: "1px solid #bbb",
-                    transform: "rotate(45deg)",
-
-                    // Horizontally center
-                    left: 0,
-                    right: 0,
-                    margin: "auto",
-                },
-            },
         }}>
             <div
                 title={title}
@@ -200,121 +120,5 @@ const ActionIcon: React.FC<ActionIconProps> = ({ title, onClick, isActive, child
                 }}
             >{children}</div>
         </div>
-    );
-};
-
-/** The main menu with several links and functions. */
-const MainMenu: React.FC<{ closeMenu: () => void }> = ({ closeMenu }) => {
-    const { t } = useTranslation();
-    const iconStyle = {
-        display: "inline-block",
-        marginRight: 16,
-        color: "#666",
-    };
-
-    const itemStyle = {
-        display: "block",
-        whiteSpace: "nowrap" as const,
-        padding: "6px 8px 6px 12px",
-        textDecoration: "none",
-        "&:hover": {
-            backgroundColor: "#ddd",
-        },
-    };
-
-    return <>
-        <div css={{
-            borderBottom: "1px solid #bbb",
-            textAlign: "center",
-            minWidth: 220,
-            padding: 8,
-        }}>
-            {t("login.not-logged-in")}
-            <LoginButton onClick={closeMenu}/>
-        </div>
-        <ul css={{
-            width: "100%",
-            listStyle: "none",
-            margin: 0,
-            padding: 0,
-            fontSize: 18,
-        }}>
-            <li css={itemStyle}>
-                <FontAwesomeIcon icon={faMoon} fixedWidth css={iconStyle} />
-                {t("main-menu.theme")}
-            </li>
-            <li>
-                <Link to="/settings" css={itemStyle} onClick={closeMenu}>
-                    <FontAwesomeIcon icon={faCog} fixedWidth css={iconStyle} />
-                    {t("main-menu.settings")}
-                </Link>
-            </li>
-            <li>
-                <Link to="/about" css={itemStyle} onClick={closeMenu}>
-                    <FontAwesomeIcon icon={faInfoCircle} fixedWidth css={iconStyle} />
-                    {t("main-menu.about")}
-                </Link>
-            </li>
-        </ul>
-    </>;
-};
-
-const LoginButton: React.FC<{ onClick: () => void }> = ({ onClick }) => {
-    const { t } = useTranslation();
-
-    return (
-        <Link
-            to="/login"
-            onClick={onClick}
-            css={{
-                display: "inline-block",
-                marginTop: 8,
-                padding: "4px 8px",
-                fontSize: 18,
-                textDecoration: "none",
-                whiteSpace: "nowrap",
-                border: "2px solid #495dce",
-                borderRadius: 3,
-                "&:hover": {
-                    boxShadow: "0 0 5px #0000004d",
-                },
-            }}
-        >
-            {t("login.button-label")}
-        </Link>
-    );
-};
-
-const LanguageList = () => {
-    const { t, i18n } = useTranslation();
-    const currentLng = i18n.languages.find(lng => Object.keys(languages).includes(lng));
-
-    return (
-        <ul css={{
-            width: "100%",
-            listStyle: "none",
-            fontSize: 18,
-            margin: 0,
-            padding: 0,
-        }}>
-            {Object.keys(languages).map(lng => (
-                <li
-                    onClick={() => i18n.changeLanguage(lng)}
-                    key={lng}
-                    css={{
-                        padding: "6px 8px 6px 12px",
-                        cursor: "pointer",
-                        "&:hover": {
-                            backgroundColor: "#ddd",
-                        },
-                    }}
-                >
-                    <div className="fa-fw" css={{ display: "inline-block", marginRight: 16 }}>
-                        {currentLng === lng && <FontAwesomeIcon icon={faCheck} fixedWidth />}
-                    </div>
-                    {t("language-name", { lng })}
-                </li>
-            ))}
-        </ul>
     );
 };
