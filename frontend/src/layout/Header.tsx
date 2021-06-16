0ac1789cbfefa1fb8cfe6ba21ee5cb3f7e8a3263
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-06-16 20:53:40 +0200

    Show search bar on large screens only, showing an icon otherwise

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faBars, faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { useTranslation } from "react-i18next";
 
 import CONFIG from "../config";
@@ -15,21 +15,62 @@ type Props = {
     setBurgerVisible: (visible: boolean) => void;
 };
 
-export const Header: React.FC<Props> = ({ burgerVisible, setBurgerVisible }) => (
-    <header css={{
-        height: "var(--header-height)",
-        display: "flex",
-        padding: "var(--header-padding) min(5vw, var(--header-padding))",
-        marginBottom: "16px",
-        alignItems: "center",
-        justifyContent: "space-between",
-        backgroundColor: "white",
-    }}>
-        <Logo />
-        {!burgerVisible && <Search />}
-        <ActionIcons setBurgerVisible={setBurgerVisible} burgerVisible={burgerVisible} />
-    </header>
-);
+export const Header: React.FC<Props> = ({ burgerVisible, setBurgerVisible }) => {
+    const { t } = useTranslation();
+    const [searchActive, setSearchActive] = useState(false);
+
+    if (searchActive && burgerVisible) {
+        console.log("unexpected state: search active and burger visible");
+    }
+
+    const content = (() => {
+        if (searchActive) {
+            return <>
+                <ActionIcon title={t("back")} onClick={() => setSearchActive(false)} >
+                    <FontAwesomeIcon icon={faArrowLeft} />
+                </ActionIcon>
+                <SearchField variant="mobile" />
+            </>;
+        } else {
+            return <>
+                <Logo />
+                {!burgerVisible && <SearchField variant="desktop" />}
+                <div css={{ display: "flex", height: "100%", position: "relative" }}>
+                    {!burgerVisible && <ActionIcon
+                        title={t("search")}
+                        onClick={() => setSearchActive(true)}
+                        smallScreensOnly={true}
+                    >
+                        <FontAwesomeIcon icon={faSearch} fixedWidth />
+                    </ActionIcon>}
+
+                    <ActionIcon
+                        title={t("main-menu.label")}
+                        onClick={() => setBurgerVisible(!burgerVisible)}
+                    >
+                        {burgerVisible
+                            ? <FontAwesomeIcon icon={faTimes} fixedWidth />
+                            : <FontAwesomeIcon icon={faBars} fixedWidth />}
+                    </ActionIcon>
+                </div>
+            </>;
+        }
+    })();
+
+    return (
+        <header css={{
+            height: "var(--header-height)",
+            display: "flex",
+            padding: "var(--header-padding) min(5vw, var(--header-padding))",
+            marginBottom: "16px",
+            alignItems: "center",
+            justifyContent: "space-between",
+            backgroundColor: "white",
+        }}>
+            {content}
+        </header>
+    );
+};
 
 const Logo: React.FC = () => (
     <Link to="/" css={{ height: "100%", flex: "0 1 auto" }}>
@@ -45,9 +86,25 @@ const Logo: React.FC = () => (
 
 
 const SEARCH_HEIGHT = 35;
+const SEARCH_BREAKPOINT = 600;
 
-const Search: React.FC = () => {
+type SearchFieldProps = {
+    variant: "desktop" | "mobile";
+};
+
+const SearchField: React.FC<SearchFieldProps> = ({ variant }) => {
     const { t } = useTranslation();
+
+    const extraCss = variant === "desktop"
+        ? {
+            maxWidth: 280,
+            [`@media (max-width: ${SEARCH_BREAKPOINT}px)`]: {
+                display: "none",
+            }
+        }
+        : {
+            width: "100%",
+        };
 
     return (
         <input
@@ -57,47 +114,30 @@ const Search: React.FC = () => {
                 flex: "1 1 0px",
                 margin: "0 8px",
                 minWidth: 50,
-                maxWidth: 280,
                 height: SEARCH_HEIGHT,
                 borderRadius: SEARCH_HEIGHT / 2,
                 border: "1.5px solid #ccc",
                 padding: `0 ${SEARCH_HEIGHT / 2}px`,
+                ...extraCss
             }}
         />
     );
 };
 
 
-type ActionButtonProps = {
-    burgerVisible: boolean;
-    setBurgerVisible: (visible: boolean) => void;
-};
-
-/** The icons on the right side of the header: changing language and main menu. */
-const ActionIcons: React.FC<ActionButtonProps> = ({ setBurgerVisible, burgerVisible }) => {
-    const { t } = useTranslation();
-
-    return (
-        <div css={{ display: "flex", height: "100%", position: "relative" }}>
-            <ActionIcon
-                title={t("main-menu.label")}
-                onClick={() => setBurgerVisible(!burgerVisible)}
-            >
-                {burgerVisible
-                    ? <FontAwesomeIcon icon={faTimes} fixedWidth />
-                    : <FontAwesomeIcon icon={faBars} fixedWidth />}
-            </ActionIcon>
-        </div>
-    );
-};
-
 type ActionIconProps = {
     onClick: () => void;
     title: string;
+    smallScreensOnly?: boolean;
 };
 
 /** A single button with icon in the header. */
-const ActionIcon: React.FC<ActionIconProps> = ({ title, onClick, children }) => {
+const ActionIcon: React.FC<ActionIconProps> = ({
+    title,
+    onClick,
+    smallScreensOnly = false,
+    children,
+}) => {
     const iconSize = 28;
 
     return (
@@ -105,6 +145,12 @@ const ActionIcon: React.FC<ActionIconProps> = ({ title, onClick, children }) => 
             height: "100%",
             display: "flex",
             alignItems: "center",
+            ...smallScreensOnly && {
+                display: "none",
+                [`@media (max-width: ${SEARCH_BREAKPOINT}px)`]: {
+                    display: "flex",
+                },
+            }
         }}>
             <div
                 title={title}
