61608613d71fa1f1c5825f1bd6c8b453a2bd2ee8
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-07-16 16:35:21 +0200

    Add loading indicator similar to the one on YouTube or GitHub
    This is only for non-initial loads!

--- frontend/src/ui/Blocks.tsx
+++ frontend/src/ui/Blocks.tsx
@@ -81,8 +81,8 @@ function unwrap<K extends keyof BlockData>(block: BlockData, field: K): NonNulla
 
     const v = block[field];
     if (!isNotNullish(v)) {
-        return unreachable(`field '${field}' of block is null, but that should ` +
-            `never happen for the type '${block.__typename}'`);
+        return unreachable(`field '${field}' of block is null, but that should `
+            + `never happen for the type '${block.__typename}'`);
     }
 
     return v;
