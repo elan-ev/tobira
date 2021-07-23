c0344865000970e8bbf6d1d647f236314c0d1728
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-07-23 12:24:18 +0200

    Show some metadata about video on video page

--- frontend/src/routes/Video.tsx
+++ frontend/src/routes/Video.tsx
@@ -12,6 +12,7 @@ import { navFromQuery } from "../layout/Navigation";
 import { VideoBlock } from "../ui/blocks/Video";
 import { TextBlock } from "../ui/blocks/Text";
 import type { Track } from "../ui/Player";
+import { useTranslation } from "react-i18next";
 
 
 export const VideoRoute: Route<PreloadedQuery<VideoQuery>> = {
@@ -27,6 +28,10 @@ const query = graphql`
         event(id: $id) {
             title
             description
+            creator
+            created
+            updated
+            series { title }
             tracks { uri flavor mimetype resolution }
         }
         realm: realmByPath(path: $realmPath) {
@@ -40,6 +45,7 @@ type Props = {
 };
 
 const VideoPage: React.FC<Props> = ({ queryRef }) => {
+    const { t, i18n } = useTranslation();
     const { event, realm } = usePreloadedQuery(query, queryRef);
 
     // TODO: this realm check is useless once we check a video belongs to a realm.
@@ -47,12 +53,54 @@ const VideoPage: React.FC<Props> = ({ queryRef }) => {
         return <NotFound kind="video" />;
     }
 
+    const createdDate = new Date(event.created);
+    const created = createdDate.toLocaleString(i18n.language);
+
+    // If the event was updated only shortly after the creation date, we don't
+    // want to show it.
+    const updatedDate = new Date(event.updated);
+    const updated = updatedDate.getTime() - createdDate.getTime() > 5 * 60 * 1000
+        ? updatedDate.toLocaleString(i18n.language)
+        : null;
+
     const { title, tracks, description } = event;
     return (
         <Root navSource={navFromQuery(realm)}>
             <h1>{title}</h1>
             <VideoBlock tracks={tracks as Track[]} />
             {description !== null && <TextBlock content={description} />}
+            <table css={{
+                marginBottom: 100,
+                "& tr": {
+                    "& > td:first-child": {
+                        color: "var(--grey40)",
+                        paddingRight: 32,
+                    },
+                },
+            }}>
+                <tbody>
+                    <MetaDatum label={t("video.creator")} value={event.creator} />
+                    <MetaDatum label={t("video.created")} value={created} />
+                    <MetaDatum label={t("video.updated")} value={updated} />
+                    <MetaDatum label={t("video.part-of-series")} value={event.series?.title} />
+                </tbody>
+            </table>
         </Root>
     );
+};
+
+type MetaDatumProps = {
+    label: string;
+    value: string | null | undefined;
+};
+
+const MetaDatum: React.FC<MetaDatumProps> = ({ label, value }) => {
+    if (value == null) {
+        return null;
+    }
+
+    return <tr>
+        <td>{label}:</td>
+        <td>{value}</td>
+    </tr>;
 };
