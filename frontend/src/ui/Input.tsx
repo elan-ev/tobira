1dd856ea391a62181976703b2e641ec3ee3d48c1
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-08-03 18:23:19 +0200

    Fix `Input` component by properly forwarding `ref`

--- frontend/src/ui/Input.tsx
+++ frontend/src/ui/Input.tsx
@@ -1,21 +1,26 @@
+import React from "react";
+
 type Props = JSX.IntrinsicElements["input"] & {
     error?: boolean;
 };
 
 
 /** A styled single-line text box */
-export const Input: React.FC<Props> = ({ error = false, ...rest }) => (
-    <input
-        css={{
-            borderRadius: 4,
-            padding: "4px 10px",
-            border: `1px solid ${error ? "var(--danger-color)" : "var(--grey80)"}`,
-            "&:focus": {
-                outline: "none",
-                boxShadow: "0 0 0 1px var(--accent-color)",
-                borderColor: "var(--accent-color)",
-            },
-        }}
-        {...rest}
-    />
+export const Input = React.forwardRef<HTMLInputElement, Props>(
+    ({ error = false, ...rest }, ref) => (
+        <input
+            ref={ref}
+            css={{
+                borderRadius: 4,
+                padding: "4px 10px",
+                border: `1px solid ${error ? "var(--danger-color)" : "var(--grey80)"}`,
+                "&:focus": {
+                    outline: "none",
+                    boxShadow: "0 0 0 1px var(--accent-color)",
+                    borderColor: "var(--accent-color)",
+                },
+            }}
+            {...rest}
+        />
+    ),
 );
