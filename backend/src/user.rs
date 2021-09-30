6fb3cc2df6e5933665e848fb62847c9c2c30fce1
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-09-30 13:43:26 +0200

    Ignore user-related header values if `auth.proxy.enabled` is false

--- backend/src/user.rs
+++ backend/src/user.rs
@@ -14,6 +14,11 @@ pub(crate) struct User {
 
 impl User {
     pub(crate) fn from_headers(headers: &HeaderMap, auth_config: &AuthConfig) -> Option<Self> {
+        // We only read these header values if the auth proxy is enabled.
+        if !auth_config.proxy.enabled {
+            return None;
+        }
+
         let as_utf8 = |v: &HeaderValue| String::from_utf8_lossy(v.as_bytes()).trim().to_owned();
         let username = as_utf8(headers.get(&auth_config.username_header)?);
         let display_name = as_utf8(headers.get(&auth_config.display_name_header)?);
