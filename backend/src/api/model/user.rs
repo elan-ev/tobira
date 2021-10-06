4d5599900adbe0171aad439afb0e9e3f11f0b7f2
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-10-06 18:07:24 +0200

    Remove `roles`, `isModerator` and `isAdmin` fields from `User` in API
    Having these "low level authorization" fields encourages putting
    authorization logic into the frontend. But that's not what we want. The
    logic has to be in the backend anyway, so we want to have one specific
    endpoint for each authorized action we need to check.

--- backend/src/api/model/user.rs
+++ backend/src/api/model/user.rs
@@ -15,19 +15,4 @@ impl User {
     fn display_name(&self) -> &str {
         &self.display_name
     }
-
-    /// Returns all roles of this user.
-    fn roles(&self) -> &[String] {
-        &self.roles
-    }
-
-    /// Returns whether this user is a moderator.
-    fn is_moderator(&self, context: &Context) -> bool {
-        self.is_moderator(&context.config.auth)
-    }
-
-    /// Returns whether this user is an admin.
-    fn is_admin(&self) -> bool {
-        self.is_admin()
-    }
 }
