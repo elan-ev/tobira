c5d39fe5543dfb8a6e8d4af45d946927a52a697b
Julian Kniephoff <kniephoff@elan-ev.de>
2021-11-22 14:34:00 +0100

    First draft of move functionality in the frontend

--- frontend/src/routes/manage/Realm/Content/Edit/MoveButtons.tsx
+++ frontend/src/routes/manage/Realm/Content/Edit/MoveButtons.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { useFragment, graphql } from "react-relay";
+import { useFragment, graphql, useMutation } from "react-relay";
 import { FiArrowDown, FiArrowUp } from "react-icons/fi";
 
 import type { MoveButtonsData$key } from "../../../../../query-types/MoveButtonsData.graphql";
@@ -15,19 +15,49 @@ type Props = {
 export const MoveButtons: React.FC<Props> = ({ realm, index }) => {
     const { t } = useTranslation();
 
-    const { blocks } = useFragment(graphql`
+
+    const { id: realmId, blocks } = useFragment(graphql`
         fragment MoveButtonsData on Realm {
+            id
             # We need this list only for the length,
             # but we have to query *something* from it.
             blocks { id }
         }
     `, realm);
 
+
+    const [commitMove] = useMutation(graphql`
+        mutation MoveButtonsMutation($realmId: ID!, $index1: Int!, $index2: Int!) {
+            swapBlocksByIndex(realm: $realmId, index1: $index1, index2: $index2) {
+                ... ContentManageData
+            }
+        }
+    `);
+
+    const move = (direction: -1 | 1) => {
+        commitMove({
+            variables: {
+                realmId,
+                index1: index,
+                index2: index + direction,
+            },
+        });
+    };
+
+
     return <>
-        <Button title={t("manage.realm.content.move-down")} disabled={index === blocks.length - 1}>
+        <Button
+            title={t("manage.realm.content.move-down")}
+            disabled={index === blocks.length - 1}
+            onClick={() => move(1)}
+        >
             <FiArrowDown />
         </Button>
-        <Button title={t("manage.realm.content.move-up")} disabled={index === 0}>
+        <Button
+            title={t("manage.realm.content.move-up")}
+            disabled={index === 0}
+            onClick={() => move(-1)}
+        >
             <FiArrowUp />
         </Button>
     </>;
