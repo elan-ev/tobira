6c8fffc4c6b1c16158398f7ffb66395105703e04
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-11-24 11:41:23 +0100

    Add functions to perform JWT-authenticated requests to Opencast

--- frontend/src/routes/Upload.tsx
+++ frontend/src/routes/Upload.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { graphql, usePreloadedQuery } from "react-relay";
+import { graphql, usePreloadedQuery, useRelayEnvironment } from "react-relay";
 import type { PreloadedQuery } from "react-relay";
 
 import { Root } from "../layout/Root";
@@ -8,6 +8,10 @@ import { loadQuery } from "../relay";
 import { UploadQuery } from "../query-types/UploadQuery.graphql";
 import { UPLOAD_PATH } from "./paths";
 import { makeRoute } from "../rauta";
+import { Environment, fetchQuery } from "relay-runtime";
+import { UploadJwtQuery } from "../query-types/UploadJwtQuery.graphql";
+import { bug } from "../util/err";
+import CONFIG from "../config";
 
 
 export const UploadRoute = makeRoute<PreloadedQuery<UploadQuery>>({
@@ -31,12 +35,69 @@ type Props = {
 const Upload: React.FC<Props> = ({ queryRef }) => {
     const { t } = useTranslation();
     const result = usePreloadedQuery(query, queryRef);
+    const relayEnv = useRelayEnvironment();
+
+    const start = async () => {
+        // TODO: error handling
+        const res = await ocRequest(relayEnv, "/info/me.json");
+        console.log(await res.json());
+    };
 
     return (
         <Root nav={[]} userQuery={result}>
             <div css={{ margin: "0 auto", maxWidth: 600 }}>
                 <h1>{t("upload.title")}</h1>
+                <button onClick={start}>Start</button>
             </div>
         </Root>
     );
 };
+
+/** Performs a request against Opencast, authenticated via JWT */
+const ocRequest = async (
+    relayEnv: Environment,
+    path: string,
+    options: RequestInit = {},
+): Promise<Response> => {
+    const jwt = await getJwt(relayEnv);
+
+    const url = `${CONFIG.ocUrl}${path}`;
+    return await fetch(url, {
+        ...options,
+        headers: {
+            ...options.headers,
+            "Authorization": `Bearer ${jwt}`,
+        },
+    });
+};
+
+/** Fetches a new JWT for uploading to Opencast */
+const getJwt = (relayEnv: Environment): Promise<string> => new Promise((resolve, reject) => {
+    const query = graphql`
+        query UploadJwtQuery {
+            uploadJwt
+        }
+    `;
+
+    let out: string | null = null;
+
+    // Use "network-only" as we always want a fresh JWTs. `fetchQuery` should already
+    // never write any values into the cache, but better make sure.
+    fetchQuery<UploadJwtQuery>(relayEnv, query, {}, { fetchPolicy: "network-only" }).subscribe({
+        complete: () => {
+            if (out === null) {
+                bug("'complete' callback before receiving any data");
+            } else {
+                resolve(out);
+            }
+        },
+        error: (error: unknown) => reject(error),
+        next: data => {
+            if (out !== null) {
+                bug("unexpected second data when retrieving JWT");
+            } else {
+                out = data.uploadJwt;
+            }
+        },
+    });
+});
