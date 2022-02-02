5814effb49f233b2b0bb2281897fecb36bfd6ffe
Julian Kniephoff <kniephoff@elan-ev.de>
2022-02-02 15:06:22 +0100

    First draft of actually rendering video blocks

--- frontend/src/ui/Blocks/Video.tsx
+++ frontend/src/ui/Blocks/Video.tsx
@@ -1,9 +1,40 @@
-import { BlockContainer } from ".";
-import { Player, PlayerProps } from "../player";
+import { graphql, useFragment } from "react-relay";
+
+import { Track, Player } from "../player";
+import { VideoBlockData$key } from "./__generated__/VideoBlockData.graphql";
+import { Title, BlockContainer } from ".";
 
 
-export const VideoBlock: React.FC<PlayerProps> = props => (
-    <BlockContainer>
-        <Player {...props} />
-    </BlockContainer>
-);
+type Props = {
+    title?: string;
+    fragRef: VideoBlockData$key;
+};
+
+export const VideoBlock: React.FC<Props> = ({ title, fragRef }) => {
+    const { event } = useFragment(graphql`
+        fragment VideoBlockData on VideoBlock {
+            event {
+                title
+                duration
+                tracks {
+                    uri
+                    flavor
+                    mimetype
+                    resolution
+                }
+            }
+        }
+    `, fragRef);
+
+    return <BlockContainer>
+        {/* TODO The title display logic will change soon */}
+        <Title title={title ?? event.title} />
+        <Player
+            {...event}
+            // TODO In the future, duration won't be nullable anymore
+            duration={event.duration ?? 0}
+            // Relay returns `readonly` objects ...
+            tracks={event.tracks as Track[]}
+        />
+    </BlockContainer>;
+};
