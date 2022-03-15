06d57529a37cc03791b79ec62265bbb778826c3d
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2022-03-15 18:32:36 +0100

    Extract HTTP host logic from sync config
    This puts the protocol and host/port into one field. That field can
    check its safety itself. That is useful for other fields like this
    in the future.

--- backend/src/sync/mod.rs
+++ backend/src/sync/mod.rs
@@ -1,14 +1,11 @@
 use secrecy::Secret;
-use std::{net::{IpAddr, ToSocketAddrs}, time::Duration};
+use std::time::Duration;
 
-use crate::{config::Config, db, prelude::*};
+use crate::{config::Config, db, prelude::*, util::HttpHost};
 
 
 mod harvest;
 mod status;
-
-#[cfg(test)]
-mod tests;
 
 
 pub(crate) async fn run(daemon: bool, config: &Config) -> Result<()> {
@@ -31,17 +28,15 @@ pub(crate) async fn run(daemon: bool, config: &Config) -> Result<()> {
 #[derive(Debug, confique::Config)]
 pub(crate) struct SyncConfig {
     /// Host of the connected Opencast instance. This host has to be reachable
-    /// via HTTPS (or HTTP, see `use_insecure_connection`). If no port is
-    /// specified here, the default HTTPS port 443 (or HTTP port 80) is used.
+    /// via HTTPS or HTTP. If no port is specified here, the default HTTPS port
+    /// 443 (or HTTP port 80) is used.
+    ///
+    /// The HTTP requests to Opencast contain the unencrypted `password`, so
+    /// using HTTPS is strongly encouraged. In fact, HTTP is only allowed if
+    /// the host resolves to a loopback address.
+    ///
     /// Example: "localhost:8080".
-    host: String,
-
-    /// If set to `true`, Tobira will communicate with Opencast via HTTP instead of
-    /// HTTPS. This is strongly recommended against! The HTTP requests contain the
-    /// unencrypted `sync_password`! Setting this to `true` is only allowed if
-    /// the `host` resolves to a loopback address.
-    #[config(default = false)]
-    use_insecure_connection: bool,
+    pub(crate) host: HttpHost,
 
     /// Username of the user used to communicate with Opencast. This user has to have
     /// access to all events and series. Currently, that user has to be admin.
@@ -75,58 +70,8 @@ pub(crate) struct SyncConfig {
 }
 
 impl SyncConfig {
-    pub(crate) fn oc_url(&self) -> String {
-        format!(
-            "{}://{}",
-            if self.use_insecure_connection { "http" } else { "https" },
-            self.host,
-        )
-    }
-
     pub(crate) fn validate(&self) -> Result<()> {
-        let host_as_ip = self.host.parse::<IpAddr>();
-
-        // We only allow HTTP if the host resolves to a loopback (local)
-        // address. We send the unencrypted `sync_password`, so HTTPS is
-        // required.
-        if self.use_insecure_connection {
-            debug!("Checking whether Opencast host '{}' is a loopback address", self.host);
-
-            let is_loopback = if let Ok(addr) = host_as_ip {
-                addr.is_loopback()
-            } else {
-                let mut socket_addrs = if self.host.contains(':') {
-                    // If the host is not parsable as an IPv6 address (checked
-                    // above), a colon means that the port is included in the
-                    // string.
-                    self.host.to_socket_addrs()?
-                } else {
-                    (&*self.host, 80u16).to_socket_addrs()?
-                };
-
-                socket_addrs.all(|sa| sa.ip().is_loopback())
-            };
-
-            if !is_loopback {
-                bail!(
-                    "`opencast.use_insecure_connection` is set to `true`, but \
-                        `opencast.host` ('{}') is not/does not resolve to a loopback address. \
-                        For security, this is not allowed.",
-                    self.host,
-                );
-            }
-        }
-
-        // Check that the host field is either a valid IP addr or a valid host.
-        // That's not quite the same for IPv6, as those have to be enclosed in
-        // `[]` in a URI.
-        if host_as_ip.is_err() {
-            // TODO: this should be a custom parser or whatever so that the
-            // struct can hold an `Authority`. Blocked by "config lib".
-            self.host.parse::<hyper::http::uri::Authority>()
-                .context("'opencast.host' is not a valid URI authority")?;
-        }
-
+        self.host.assert_safety().context("failed to validate 'sync.host'")?;
         Ok(())
     }
 }
--- backend/src/util.rs
+++ backend/src/util.rs
@@ -1,4 +1,8 @@
-use std::fmt;
+use std::{fmt, str::FromStr, net::Ipv6Addr};
+use serde::Deserialize;
+
+use crate::prelude::*;
+
 
 /// A lazy `fmt` formatter, specified by a callable. Usually created via
 /// `lazy_format!`.
@@ -26,3 +30,142 @@ macro_rules! lazy_format {
 }
 
 pub(crate) use lazy_format;
+
+
+#[derive(Clone, Deserialize)]
+#[serde(try_from = "String")]
+pub(crate) struct HttpHost {
+    pub(crate) scheme: hyper::http::uri::Scheme,
+    pub(crate) authority: hyper::http::uri::Authority,
+}
+
+impl HttpHost {
+    /// Makes sure that the scheme is HTTPS or that the host resolves to a loopback address.
+    pub(crate) fn assert_safety(&self) -> Result<()> {
+        use std::net::ToSocketAddrs;
+
+        if self.scheme == hyper::http::uri::Scheme::HTTP {
+            let host = self.authority.host();
+
+            let bracketed_ipv6 =
+                (|| host.strip_prefix('[')?.strip_suffix(']')?.parse::<Ipv6Addr>().ok())();
+            let is_loopback = match bracketed_ipv6 {
+                Some(ipv6) => ipv6.is_loopback(),
+                None => (host, 0u16).to_socket_addrs()
+                    .context("failed to resolve host")?
+                    .all(|sa| sa.ip().is_loopback()),
+            };
+
+            anyhow::ensure!(
+                is_loopback,
+                "Host '{self}' uses unsecure HTTP, but does not resolve to a loopback \
+                    address. For security, this is not allowed.",
+            );
+        }
+
+        Ok(())
+    }
+}
+
+impl fmt::Display for HttpHost {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}://{}", self.scheme, self.authority)
+    }
+}
+
+impl fmt::Debug for HttpHost {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        fmt::Display::fmt(self, f)
+    }
+}
+
+impl FromStr for HttpHost {
+    type Err = anyhow::Error;
+    fn from_str(src: &str) -> Result<Self, Self::Err> {
+        let parts = src.parse::<hyper::http::uri::Uri>()?.into_parts();
+        let has_real_path = parts.path_and_query.as_ref()
+            .map_or(false, |pq| !pq.as_str().is_empty() && pq.as_str() != "/");
+        if has_real_path {
+            bail!("invalid HTTP host: must not contain a path");
+        }
+
+        let authority = parts.authority
+            .ok_or(anyhow!("invalid HTTP host: contains no authority part"))?;
+        let scheme = parts.scheme
+            .ok_or(anyhow!("invalid HTTP host: has to specify 'http' or 'https'"))?;
+
+        if !authority.as_str().starts_with(authority.host()) {
+            bail!("userinfo not allowed in authority");
+        }
+
+        Ok(Self { scheme, authority })
+    }
+}
+
+impl TryFrom<String> for HttpHost {
+    type Error = <Self as FromStr>::Err;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        value.parse()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::HttpHost;
+
+    fn parse_http_host(s: &str) -> HttpHost {
+        s.parse::<HttpHost>().expect(&format!("could not parse '{s}' as HttpHost"))
+    }
+
+    #[test]
+    fn http_hosts_loopback() {
+        let hosts = [
+            "localhost",
+            "localhost:1234",
+            "127.0.0.1",
+            "127.0.0.1:4321",
+            "127.1.2.3",
+            "127.1.2.3:4321",
+            "[::1]",
+            "[::1]:4321",
+        ];
+
+        for host in hosts {
+            for scheme in ["http", "https"] {
+                let http_host = parse_http_host(&format!("{scheme}://{host}"));
+                if let Err(e) = http_host.assert_safety() {
+                    panic!("Failed to validate {http_host}: {e}");
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn http_hosts_non_loopback() {
+        let hosts = [
+            "1.1.1.1",
+            "1.1.1.1:3456",
+            "[2606:4700:4700::1111]",
+            "[2606:4700:4700::1111]:3456",
+            "github.com",
+            "github.com:3456",
+        ];
+
+        for host in hosts {
+            for scheme in ["http", "https"] {
+                let http_host = parse_http_host(&format!("{scheme}://{host}"));
+
+                if scheme == "http" {
+                    if http_host.assert_safety().is_ok() {
+                        panic!("HttpHost validated successfully, but shouldn't! {http_host}");
+                    }
+                } else {
+                    if let Err(e) = http_host.assert_safety() {
+                        panic!("Failed to validate {http_host}: {e}");
+                    }
+                }
+            }
+        }
+    }
+}
