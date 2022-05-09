4ba5c1cd359e2afcd0ee1fe1ba94ec3365094b01
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2022-05-09 13:15:43 +0200

    Redesign metadata part of video page
    The old design looked a bit dull and was just some "dump all the data
    somehow". While the new design is not perfect either, I think it's a
    good starting point and already looks better. With this, we can display
    arbitrary metadata.

--- frontend/src/routes/Video.tsx
+++ frontend/src/routes/Video.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode, useState } from "react";
 import { graphql } from "react-relay/hooks";
 
 import type { VideoQuery, VideoQuery$data } from "./__generated__/VideoQuery.graphql";
@@ -6,17 +6,17 @@ import { loadQuery } from "../relay";
 import { RootLoader } from "../layout/Root";
 import { NotFound } from "./NotFound";
 import { Nav } from "../layout/Navigation";
-import { TextBlock } from "../ui/Blocks/Text";
 import { Player, Track } from "../ui/player";
 import { useTranslation } from "react-i18next";
 import { SeriesBlockFromReadySeries } from "../ui/Blocks/Series";
 import { makeRoute, MatchedRoute } from "../rauta";
-import { Link } from "../router";
-import { FiChevronRight } from "react-icons/fi";
 import { isValidPathSegment } from "./Realm";
 import { Breadcrumbs } from "../ui/Breadcrumbs";
 import { PageTitle } from "../layout/header/ui";
 import { unreachable } from "../util/err";
+import { BREAKPOINT_SMALL, BREAKPOINT_MEDIUM } from "../GlobalStyle";
+import { HiOutlineUserCircle } from "react-icons/hi";
+import { LinkButton } from "../ui/Button";
 
 
 export const b64regex = "[a-zA-Z0-9\\-_]";
@@ -103,6 +103,7 @@ const query = graphql`
             duration
             thumbnail
             isLive
+            metadata
             canWrite
             series { title ... SeriesBlockReadySeriesData }
             tracks { uri flavor mimetype resolution }
@@ -126,19 +127,7 @@ type Props = {
 };
 
 const VideoPage: React.FC<Props> = ({ event, realm, id, basePath }) => {
-    const { t, i18n } = useTranslation();
-
-    const createdDate = new Date(event.created);
-    const created = createdDate.toLocaleString(i18n.language);
-
-    // If the event was updated only shortly after the creation date, we don't
-    // want to show it.
-    const updatedDate = new Date(event.updated);
-    const updated = updatedDate.getTime() - createdDate.getTime() > 5 * 60 * 1000
-        ? updatedDate.toLocaleString(i18n.language)
-        : null;
-
-    const { title, tracks, description } = event;
+    const { t } = useTranslation();
 
     const breadcrumbs = (realm.isRoot ? realm.ancestors : realm.ancestors.concat(realm))
         .map(({ name, path }) => ({ label: name, link: path }));
@@ -146,42 +135,14 @@ const VideoPage: React.FC<Props> = ({ event, realm, id, basePath }) => {
     return <>
         <Breadcrumbs path={breadcrumbs} tail={event.title} />
         <Player
-            tracks={tracks as Track[]}
-            title={title}
+            tracks={event.tracks as Track[]}
+            title={event.title}
             isLive={event.isLive}
             duration={event.duration}
             coverImage={event.thumbnail}
             css={{ margin: "0 auto" }}
         />
-        <PageTitle title={title} css={{ marginTop: 24, fontSize: 24 }} />
-        {description !== null && <TextBlock content={description} />}
-        <table css={{
-            marginBottom: 16,
-            "& tr": {
-                "& > td:first-child": {
-                    color: "var(--grey40)",
-                    paddingRight: 32,
-                },
-            },
-        }}>
-            <tbody>
-                {/* TODO: improve upon join */}
-                <MetaDatum label={t("video.creator")} value={event.creators.join(", ")} />
-                <MetaDatum label={t("video.created")} value={created} />
-                <MetaDatum label={t("video.updated")} value={updated} />
-                <MetaDatum label={t("video.part-of-series")} value={event.series?.title} />
-            </tbody>
-        </table>
-
-        {event.canWrite && (
-            <Link
-                to={`/~manage/videos/${id.slice(2)}`}
-                css={{ display: "inline-flex", alignItems: "center" }}
-            >
-                {t("manage.my-videos.manage-video")}
-                <FiChevronRight css={{ fontSize: 22 }} />
-            </Link>
-        )}
+        <Metadata id={id} event={event} />
 
         <div css={{ height: 80 }} />
 
@@ -194,18 +155,228 @@ const VideoPage: React.FC<Props> = ({ event, realm, id, basePath }) => {
     </>;
 };
 
-type MetaDatumProps = {
-    label: string;
-    value: string | null | undefined;
+type MetadataProps = {
+    id: string;
+    event: NonNullable<VideoQuery$data["event"]>;
 };
 
-const MetaDatum: React.FC<MetaDatumProps> = ({ label, value }) => {
-    if (value == null) {
+const Metadata: React.FC<MetadataProps> = ({ id, event }) => {
+    const { t } = useTranslation();
+
+    return <>
+        <div css={{ display: "flex", alignItems: "center", marginTop: 24 }}>
+            <div css={{ flex: "1" }}>
+                <VideoTitle title={event.title} />
+                <VideoDate created={event.created} updated={event.updated} />
+            </div>
+            <div>
+                {event.canWrite && (
+                    <LinkButton to={`/~manage/videos/${id.slice(2)}`}>
+                        {t("manage.my-videos.manage-video")}
+                    </LinkButton>
+                )}
+            </div>
+        </div>
+        <hr />
+        <div css={{
+            display: "grid",
+            gridTemplate: "1fr / 1fr fit-content(30%)",
+            columnGap: 48,
+            rowGap: 24,
+            [`@media (max-width: ${BREAKPOINT_MEDIUM}px)`]: {
+                gridTemplate: "auto auto / 1fr",
+            },
+        }}>
+            <div css={{ maxWidth: 700 }}>
+                <Creators creators={event.creators} />
+                <Description description={event.description} />
+            </div>
+            <div css={{ paddingTop: 8 }}>
+                <MetadataTable event={event} />
+            </div>
+        </div>
+
+    </>;
+};
+
+type VideoTitleProps = {
+    title: string;
+};
+
+const VideoTitle: React.FC<VideoTitleProps> = ({ title }) => (
+    <PageTitle title={title} css={{
+        marginBottom: 4,
+        fontSize: 22,
+        [`@media (max-width: ${BREAKPOINT_MEDIUM}px)`]: { fontSize: 20 },
+        [`@media (max-width: ${BREAKPOINT_SMALL}px)`]: { fontSize: 18 },
+        lineHeight: 1.2,
+
+        // Truncate title after two lines
+        display: "-webkit-box",
+        WebkitBoxOrient: "vertical",
+        textOverflow: "ellipsis",
+        WebkitLineClamp: 2,
+        overflow: "hidden",
+    }} />
+);
+
+type CreatorsProps = {
+    creators: readonly string[];
+};
+
+const Creators: React.FC<CreatorsProps> = ({ creators }) => (
+    <div css={{ display: "flex", alignItems: "center", gap: 8, marginBottom: 12 }}>
+        <HiOutlineUserCircle css={{ color: "var(--grey40)" }} />
+        <ul css={{
+            display: "inline-block",
+            listStyle: "none",
+            margin: 0,
+            padding: 0,
+            fontSize: 14,
+            fontWeight: "bold",
+            "& > li": {
+                display: "inline-block",
+                "&:not(:last-child)::after": {
+                    content: "'•'",
+                    margin: "0 8px",
+                    color: "var(--grey65)",
+                },
+            },
+        }}>
+            {creators.map((c, i) => <li key={i}>{c}</li>)}
+        </ul>
+    </div>
+);
+
+type DescriptionProps = {
+    description: string | null;
+};
+
+const Description: React.FC<DescriptionProps> = ({ description }) => {
+    if (description === null) {
         return null;
     }
 
-    return <tr>
-        <td>{label}:</td>
-        <td>{value}</td>
-    </tr>;
+    // We ignore all leading or trailing newlines and then split the whole
+    // description by empty lines (two or more consecutive newlines). That's
+    // the typical "make paragraphs from text" algorithm also used by Markdown.
+    // However, we capture those newlines to be able to output any extra
+    // (in addition to two) newlines. If a user typed many newlines in their
+    // description, they probably want to have more space there. The newlines
+    // between and within the paragraphs are then displayed via `white-space:
+    // pre-line` below.
+    const paragraphs = description.replace(/^\n*|\n*$/g, "").split(/(\n{2,})/);
+
+    // TODO: auto link URL-like things?
+    return (
+        <div css={{
+            color: "var(--grey20)",
+            fontSize: 14,
+            lineHeight: "20px",
+            whiteSpace: "pre-line",
+            "& > p:not(:first-child)": {
+                marginTop: 8,
+            },
+        }}>
+            {paragraphs.map((s, i) => i % 2 === 0
+                ? <p key={i}>{s}</p>
+                : s.slice(2))}
+        </div>
+    );
+};
+
+type VideoDateProps = {
+    created: string;
+    updated: string;
+};
+
+const VideoDate: React.FC<VideoDateProps> = props => {
+    const { t, i18n } = useTranslation();
+    const [hovering, setHovering] = useState(false);
+
+    const created = new Date(props.created);
+    const updated = new Date(props.updated);
+
+    const fullOptions = { dateStyle: "long", timeStyle: "short" } as const;
+    const createdDate = created.toLocaleDateString(i18n.language, { dateStyle: "long" });
+    const createdFull = created.toLocaleString(i18n.language, fullOptions);
+    const updatedFull = updated.getTime() - created.getTime() > 5 * 60 * 1000
+        ? updated.toLocaleString(i18n.language, fullOptions)
+        : null;
+
+    return (
+        <div
+            onMouseEnter={() => setHovering(true)}
+            onMouseLeave={() => setHovering(false)}
+            css={{
+                display: "inline-block",
+                position: "relative",
+                color: "var(--grey40)",
+                fontSize: 14,
+            }}
+        >
+            {createdDate}
+            {hovering && <div css={{
+                position: "absolute",
+                left: 2,
+                bottom: "calc(100% + 10px)",
+                width: "max-content",
+                padding: "5px 10px",
+                backgroundColor: "var(--grey86)",
+                borderRadius: 5,
+                color: "black",
+            }}>
+                <div css={{
+                    position: "absolute",
+                    width: 12,
+                    height: 12,
+                    bottom: -5,
+                    left: 20,
+                    backgroundColor: "inherit",
+                    transform: "rotate(45deg)",
+                }} />
+                <i>{t("video.created")}</i>: {createdFull}
+                <br/>
+                <i>{t("video.updated")}</i>: {updatedFull}
+            </div>}
+        </div>
+    );
+};
+
+type MetadataTableProps = {
+    event: NonNullable<VideoQuery$data["event"]>;
+};
+
+const MetadataTable: React.FC<MetadataTableProps> = ({ event }) => {
+    const { t } = useTranslation();
+
+    const pairs: [string, ReactNode][] = [];
+
+    if (event.series !== null) {
+        pairs.push([t("video.part-of-series"), event.series.title]);
+    }
+
+    // TODO: show additional metadata here
+
+    return (
+        <dl css={{
+            display: "grid",
+            columnGap: 16,
+            rowGap: 6,
+            fontSize: 14,
+            lineHeight: 1.3,
+            gridTemplateColumns: "max-content 1fr",
+            "& > dt::after": {
+                content: "':'",
+            },
+            "& > dd": {
+                color: "var(--grey40)",
+            },
+        }}>
+            {pairs.map(([label, value], i) => <React.Fragment key={i}>
+                <dt>{label}</dt>
+                <dd>{value}</dd>
+            </React.Fragment>)}
+        </dl>
+    );
 };
