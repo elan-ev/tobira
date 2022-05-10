51ea1bfce8f59d07dbfc43eeb62e700439e3b769
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2022-05-10 15:49:23 +0200

    Move more error checking code into `HarvestClient::send`
    It makes more sense there and simplifies other code.

--- backend/src/sync/harvest/client.rs
+++ backend/src/sync/harvest/client.rs
@@ -1,19 +1,18 @@
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
-use chrono::{DateTime, Utc};
+use chrono::{DateTime, Utc, TimeZone};
 use hyper::{
     Request, Body,
-    body::Bytes,
     client::{Client, HttpConnector},
-    http::{
-        response,
-        uri::{Authority, Scheme, Uri},
-    }
+    http::uri::{Authority, Scheme, Uri},
+    StatusCode
 };
 use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
 use secrecy::{ExposeSecret, Secret};
+use tap::TapFallible;
 
 use crate::{prelude::*, config::Config};
+use super::HarvestResponse;
 
 
 /// Used to send request to the harvesting API.
@@ -53,13 +52,15 @@ impl HarvestClient {
         }
     }
 
-    /// Sends a request to the harvesting API. Returns the response "meta data"
-    /// and the downloaded response body.
+    /// Sends a request to the harvesting API, checks and deserializes the
+    /// response.
     pub(super) async fn send(
         &self,
         since: DateTime<Utc>,
         preferred_amount: u64,
-    ) -> Result<(response::Parts, Bytes)> {
+    ) -> Result<HarvestResponse> {
+        let before = Instant::now();
+
         let pq = format!(
             "{}?since={}&preferredAmount={}",
             Self::API_PATH,
@@ -82,13 +83,31 @@ impl HarvestClient {
 
         debug!("Sending harvest request (since = {:?}): GET {}", since, uri);
 
-        let response = tokio::time::timeout(Duration::from_secs(60), self.http_client.request(req)).await
-            .with_context(|| format!("client request timed out"))?
-            .with_context(|| format!("failed to GET {}", uri))?;
+        let response = tokio::time::timeout(Duration::from_secs(60), self.http_client.request(req))
+            .await
+            .with_context(|| format!("Harvest request timed out (to '{uri}')"))?
+            .with_context(|| format!("Harvest request failed (to '{uri}')"))?;
 
         let (parts, body) = response.into_parts();
         let body = hyper::body::to_bytes(body).await
-            .with_context(|| format!("failed to download body from {}", uri))?;
-        Ok((parts, body))
+            .with_context(|| format!("failed to download body from '{uri}'"))?;
+
+        if parts.status != StatusCode::OK {
+            trace!("HTTP response: {:#?}", parts);
+            bail!("Harvest API returned unexpected HTTP code {}", parts.status);
+        }
+
+        let out = serde_json::from_slice::<HarvestResponse>(&body)
+            .context("Failed to deserialize response from harvesting API")
+            .tap_err(|_| trace!("HTTP response: {:#?}", parts))?;
+
+        debug!(
+            "Received {} KiB ({} items) from the harvest API (in {:.2?})",
+            body.len() / 1024,
+            out.items.len(),
+            before.elapsed(),
+        );
+
+        Ok(out)
     }
 }
