5085599e78bed0cbd9adda8dfcec3325fd18b7e1
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2022-06-30 12:36:59 +0200

    Improve "add block" UI in realm content editor
    This is basically Lisa's design, but the popup appears above instead of
    to the right. That's because the popup might otherwise overflow the
    previous <body>, which is not great. There is a lot more space above the
    + buttons.

--- frontend/src/routes/manage/Realm/Content/AddButtons.tsx
+++ frontend/src/routes/manage/Realm/Content/AddButtons.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { graphql, useFragment, commitLocalUpdate, useRelayEnvironment } from "react-relay";
 import type { RecordProxy, RecordSourceProxy } from "relay-runtime";
@@ -6,7 +6,8 @@ import { FiPlus, FiType, FiGrid, FiFilm, FiHash } from "react-icons/fi";
 
 import { AddButtonsRealmData$key } from "./__generated__/AddButtonsRealmData.graphql";
 import { bug } from "../../../../util/err";
-import { Button, ButtonGroup } from "./util";
+import { IconType } from "react-icons";
+import { useOnOutsideClick } from "../../../../util";
 
 
 type Props = {
@@ -16,6 +17,10 @@ type Props = {
 
 export const AddButtons: React.FC<Props> = ({ index, realm }) => {
     const { t } = useTranslation();
+
+    const outerRef = useRef(null);
+    const [opened, setOpened] = useState(false);
+    useOnOutsideClick(outerRef, () => setOpened(false));
 
     const { id: realmId } = useFragment(graphql`
         fragment AddButtonsRealmData on Realm {
@@ -48,38 +53,129 @@ export const AddButtons: React.FC<Props> = ({ index, realm }) => {
         });
     };
 
-    return <ButtonGroup css={{ alignSelf: "center" }}>
-        <span
-            title={t("manage.realm.content.add")}
+    const BUTTON_SIZE = 36;
+
+    return (
+        <div ref={outerRef} css={{
+            position: "relative",
+            width: BUTTON_SIZE,
+            height: BUTTON_SIZE,
+            alignSelf: "center",
+        }}>
+            <div
+                title={t("manage.realm.content.add")}
+                onClick={() => setOpened(opened => !opened)}
+                css={{
+                    cursor: "pointer",
+                    width: "100%",
+                    height: "100%",
+                    fontSize: 24,
+                    borderRadius: 4,
+                    backgroundColor: "var(--accent-color)",
+                    color: "var(--accent-color-bw-contrast)",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    "&:hover": {
+                        backgroundColor: "var(--accent-color-darker)",
+                    },
+                }}
+            >
+                <FiPlus />
+            </div>
+            {opened && <div css={{
+                position: "absolute",
+                bottom: BUTTON_SIZE + 12,
+                left: "50%",
+                transform: "translate(-50%)",
+                backgroundColor: "white",
+                boxShadow: "1px 1px 12px rgba(0, 0, 0, 30%)",
+                width: 200,
+                borderRadius: 8,
+                overflow: "hidden",
+            }}>
+                <div css={{
+                    fontSize: 14,
+                    color: "var(--grey40)",
+                    padding: "8px 16px",
+                    cursor: "default",
+                }}>{t("manage.realm.content.add-popup-title")}</div>
+                <ul css={{
+                    listStyle: "none",
+                    margin: 0,
+                    padding: 0,
+                    "& > li": {
+                        "&:not(:last-child)": {
+                            borderBottom: "1px solid var(--grey92)",
+                        },
+                    },
+                }}>
+                    <AddItem
+                        close={() => setOpened(false)}
+                        Icon={FiHash}
+                        label={t("manage.realm.content.add-title")}
+                        onClick={() => addBlock("Title")}
+                    />
+                    <AddItem
+                        close={() => setOpened(false)}
+                        Icon={FiType}
+                        label={t("manage.realm.content.add-text")}
+                        onClick={() => addBlock("Text")}
+                    />
+                    <AddItem
+                        close={() => setOpened(false)}
+                        Icon={FiGrid}
+                        label={t("manage.realm.content.add-series")}
+                        onClick={() => addBlock("Series", (_store, block) => {
+                            block.setValue("NEW_TO_OLD", "order");
+                            block.setValue(true, "showTitle");
+                        })}
+                    />
+                    <AddItem
+                        close={() => setOpened(false)}
+                        Icon={FiFilm}
+                        label={t("manage.realm.content.add-video")}
+                        onClick={() => addBlock("Video", (_store, block) => {
+                            block.setValue(true, "showTitle");
+                        })}
+                    />
+                </ul>
+            </div>}
+        </div>
+    );
+};
+
+type AddItemProps = {
+    label: string;
+    Icon: IconType;
+    onClick: () => void;
+    close: () => void;
+};
+
+const AddItem: React.FC<AddItemProps> = ({ label, Icon, onClick, close }) => (
+    <li>
+        <button
+            onClick={() => {
+                onClick();
+                close();
+            }}
             css={{
-                color: "white",
-                backgroundColor: "var(--grey20)",
+                width: "100%",
+                height: "100%",
+                padding: "6px 16px",
+                display: "flex",
+                alignItems: "center",
+                gap: 16,
+                backgroundColor: "transparent",
+                cursor: "pointer",
+                border: "none",
+                "&:hover": {
+                    backgroundColor: "var(--grey97)",
+                },
             }}
         >
-            <FiPlus />
-        </span>
-        <Button title={t("manage.realm.content.add-title")} onClick={() => addBlock("Title")}>
-            <FiHash />
-        </Button>
-        <Button title={t("manage.realm.content.add-text")} onClick={() => addBlock("Text")}>
-            <FiType />
-        </Button>
-        <Button
-            title={t("manage.realm.content.add-series")}
-            onClick={() => addBlock("Series", (_store, block) => {
-                block.setValue("NEW_TO_OLD", "order");
-                block.setValue(true, "showTitle");
-            })}
-        >
-            <FiGrid />
-        </Button>
-        <Button
-            title={t("manage.realm.content.add-video")}
-            onClick={() => addBlock("Video", (_store, block) => {
-                block.setValue(true, "showTitle");
-            })}
-        >
-            <FiFilm />
-        </Button>
-    </ButtonGroup>;
-};
+            {<Icon css={{ color: "var(--accent-color)", fontSize: 18 }}/>}
+            {label}
+        </button>
+    </li>
+);
