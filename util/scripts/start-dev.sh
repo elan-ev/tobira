440daba231e91061514c021d6db8791e31ca995f
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2022-07-19 21:58:57 +0200

    Add `start-dev` script, reimplementing main `floof` feature

---
+++ util/scripts/start-dev.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#
+# Starts the full development setup with dev-server, file-watching and
+# auto-rebuilding. Only thing this doesn't do is start any docker containers.
+
+basedir=$(dirname "$0")
+source $basedir/common-vars.sh
+
+target_port=3090
+if [[ $1 == without-login ]]; then
+    target_port=3080
+fi
+
+
+# Download frontend dependencies.
+(cd frontend && npm i --no-save)
+
+# Subshell for proper ctrl+c behavior. See 'watch-frontend.sh' for more info.
+(
+    trap 'kill 0' SIGINT;
+
+    # Start dev server for auto reload
+    penguin -p $our_port proxy localhost:$target_port &
+
+    # Build everything once and start watching for filechanges
+    $basedir/watch.sh
+)
