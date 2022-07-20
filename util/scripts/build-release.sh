a685ed28972c8a83daa8077206821193525ba79a
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2022-07-20 15:22:19 +0200

    Sanitize paths in release binary

--- util/scripts/build-release.sh
+++ util/scripts/build-release.sh
@@ -9,9 +9,24 @@ npm ci
 npx relay-compiler
 npx webpack --progress --mode=production
 
+# Build backend.
 #
+# We set some weird flags here to sanitize the paths embedded into the binary.
+# Ideally, that should happen by default, but that has not been implemented
+# yet. See this RFC: https://github.com/rust-lang/rfcs/pull/3127
+#
+# Also, the dependency 'ring' does something special with compiling C or
+# assembly files. So we also need to set the corresponding C flag. See here:
+# https://github.com/briansmith/ring/issues/715
+#
+# The long Cargo registry path is actually fixed, despite its looks. The hash in
+# there is just the one for the crates.io registry, but we don't use any other
+# crate registries, so it's fine. And yes, people could have installed cargo
+# elsewhere... we can still add support for that later.
 cd ../backend || exit 1
-cargo build --release
+CFLAGS="-fdebug-prefix-map=$HOME/.cargo/registry/src/github.com-1ecc6299db9ec823/=<dep>" \
+    RUSTFLAGS="--remap-path-prefix=$(pwd)=<src> --remap-path-prefix=$HOME/.cargo/registry/src/github.com-1ecc6299db9ec823/=<dep>" \
+    cargo build --release
 
 cd .. || exit 1
 
