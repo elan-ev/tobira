e9859b167e7b1bba47cc4a1e7f01a5ccd2d07732
Julian Kniephoff <kniephoff@elan-ev.de>
2022-07-20 13:41:43 +0200

    Make comment about waiting series more clear

--- backend/src/db/migrations/04-series.sql
+++ backend/src/db/migrations/04-series.sql
@@ -9,7 +9,7 @@ select prepare_randomized_ids('series');
 --     This can currently only happen using the `mount`-API
 --     used by the Opencast Admin UI.
 --     In this state, only the Opencast ID is valid.
---     All other (nullable) fields should be `null`,
+--     The value of all the other nullable fields is undefined,
 --     and the updated timestamp should be `-infinity`, i.e. before
 --     all other timestamps.
 -- `'ready'`: The series is fully synced and up to date, as far as
