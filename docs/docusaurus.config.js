16d89c4270f8b294e37098e6e872de05c72c0c3d
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2022-09-14 18:03:33 +0200

    Use double quotes instead of single quotes in docusaurus config
    We use double quotes everywhere.

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,23 +1,23 @@
 // @ts-check
 
-/** @type {import('@docusaurus/types').Config} */
+/** @type {import("@docusaurus/types").Config} */
 const config = {
-  title: 'Tobira documentation',
-  url: 'https://elan-ev.github.io/',
-  baseUrl: '/tobira/',
+  title: "Tobira documentation",
+  url: "https://elan-ev.github.io/",
+  baseUrl: "/tobira/",
 
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+      "classic",
+      /** @type {import("@docusaurus/preset-classic").Options} */
       ({
         docs: {
-          routeBasePath: '/',
+          routeBasePath: "/",
           sidebarCollapsed: false,
         },
         blog: false,
@@ -26,26 +26,26 @@ const config = {
   ],
 
   themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
     ({
       colorMode: {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: 'Tobira Documentation',
+        title: "Tobira Documentation",
         hideOnScroll: true,
         items: [
           {
-            href: 'https://github.com/elan-ev/tobira',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/elan-ev/tobira",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       prism: {
-        theme: require('prism-react-renderer/themes/nightOwlLight'),
-        darkTheme: require('prism-react-renderer/themes/palenight'),
-        additionalLanguages: ['toml', 'nginx'],
+        theme: require("prism-react-renderer/themes/nightOwlLight"),
+        darkTheme: require("prism-react-renderer/themes/palenight"),
+        additionalLanguages: ["toml", "nginx"],
       },
     }),
 };
