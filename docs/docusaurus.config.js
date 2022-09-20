ea2f5b324a87f7de943bfc1d11575263ed13673c
Julian Kniephoff <kniephoff@elan-ev.de>
2022-09-20 09:39:58 +0200

    Reindent docusaurus config

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -2,65 +2,65 @@
 
 /** @type {import("@docusaurus/types").Config} */
 const config = {
-  title: "Tobira documentation",
-  url: "https://elan-ev.github.io/",
-  baseUrl: "/tobira/",
-  // This is better when deploying via GH pages. See:
-  // https://docusaurus.io/docs/deployment#docusaurusconfigjs-settings
-  trailingSlash: false,
+    title: "Tobira documentation",
+    url: "https://elan-ev.github.io/",
+    baseUrl: "/tobira/",
+    // This is better when deploying via GH pages. See:
+    // https://docusaurus.io/docs/deployment#docusaurusconfigjs-settings
+    trailingSlash: false,
 
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-  },
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
 
-  presets: [
-    [
-      "classic",
-      /** @type {import("@docusaurus/preset-classic").Options} */
-      ({
-        docs: {
-          routeBasePath: "/",
-        },
-        blog: false,
-      }),
-    ],
-  ],
-
-  themes: [
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      {
-        hashed: true,
-        indexBlog: false,
-        docsRouteBasePath: "/",
-      },
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
-    ({
-      colorMode: {
-        respectPrefersColorScheme: true,
-      },
-      navbar: {
-        title: "Tobira Documentation",
-        hideOnScroll: true,
-        items: [
-          {
-            href: "https://github.com/elan-ev/tobira",
-            label: "GitHub",
-            position: "right",
-          },
+    presets: [
+        [
+            "classic",
+            /** @type {import("@docusaurus/preset-classic").Options} */
+            ({
+                docs: {
+                    routeBasePath: "/",
+                },
+                blog: false,
+            }),
         ],
-      },
-      prism: {
-        theme: require("prism-react-renderer/themes/nightOwlLight"),
-        darkTheme: require("prism-react-renderer/themes/palenight"),
-        additionalLanguages: ["toml", "nginx"],
-      },
-    }),
+    ],
+
+    themes: [
+        [
+            require.resolve("@easyops-cn/docusaurus-search-local"),
+            {
+                hashed: true,
+                indexBlog: false,
+                docsRouteBasePath: "/",
+            },
+        ],
+    ],
+
+    themeConfig:
+        /** @type {import("@docusaurus/preset-classic").ThemeConfig} */
+        ({
+            colorMode: {
+                respectPrefersColorScheme: true,
+            },
+            navbar: {
+                title: "Tobira Documentation",
+                hideOnScroll: true,
+                items: [
+                    {
+                        href: "https://github.com/elan-ev/tobira",
+                        label: "GitHub",
+                        position: "right",
+                    },
+                ],
+            },
+            prism: {
+                theme: require("prism-react-renderer/themes/nightOwlLight"),
+                darkTheme: require("prism-react-renderer/themes/palenight"),
+                additionalLanguages: ["toml", "nginx"],
+            },
+        }),
 };
 
 module.exports = config;
