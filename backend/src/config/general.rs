b424eabf171380cead5b0c21f8217d5730901643
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2022-10-24 11:22:16 +0200

    Use proper default values for list values in "general" config

--- backend/src/config/general.rs
+++ backend/src/config/general.rs
@@ -1,7 +1,5 @@
 use std::collections::HashMap;
 
-use once_cell::sync::Lazy;
-
 use super::TranslatedString;
 
 
@@ -12,16 +10,12 @@ pub(crate) struct GeneralConfig {
     // TODO: fix automatically generated `site_title =` template output.
     pub(crate) site_title: TranslatedString,
 
-    /// Links that are shown in the footer. By default, two links are shown:
+    /// Links that are shown in the footer.
     ///
-    /// ```
-    /// footer_links = ["about", "graphiql"]
-    /// ```
-    ///
-    /// By overwriting this value, you can remove the default links and add
-    /// custom ones. Note that these two default links are special and can be
-    /// specified with only the shown string. To add custom ones, you need to
-    /// define a label and a link. Example:
+    /// By overwriting this default value, you can remove the default links and
+    /// add custom ones. Note that these two default links are special and can
+    /// be specified with only the shown string. To add custom ones, you need
+    /// to define a label and a link. Example:
     ///
     /// ```
     /// footer_links = [
@@ -29,9 +23,8 @@ pub(crate) struct GeneralConfig {
     ///     "about",
     /// ]
     /// ```
-    // TODO: this shouldn't be `Option`, but `config(default = ...)` does not
-    // support complex types like this yet.
-    footer_links: Option<Vec<FooterLink>>,
+    #[config(default = ["about", "graphiql"])]
+    pub footer_links: Vec<FooterLink>,
 
     /// Additional metadata that is shown below a video. Example:
     ///
@@ -44,52 +37,43 @@ pub(crate) struct GeneralConfig {
     /// URL, there is one shortcut: the "http://purl.org/dc/terms/" is
     /// abbreviated as just "dcterms".
     ///
+    /// It's likely easier to write this as shown above (as extra section)
+    /// instead of trying to cram it into one line (as shown below with the
+    /// default value).
+    ///
     /// Instead of the manually translated label, you can use some builtin
     /// labels like this:
     ///
+    ///     [general.metadata]
     ///     dcterms.license = "builtin:license"
     ///     dcterms.source = "builtin:source"
     ///
     /// As soon as you add your own metadata fields, this default is
     /// overwritten. If you want to keep showing the license and source data,
     /// you have to add those two lines to your configuration.
-    //
-    // TODO: this shouldn't be `Option`, but `config(default = ...)` does not
-    // support complex types like this yet.
-    metadata: Option<MetadataLabels>,
+    #[config(default = {
+        "dcterms": {
+            "license": "builtin:license",
+            "source": "builtin:source",
+        }
+    })]
+    pub metadata: MetadataLabels,
 
     /// A list of URL paths that are reserved for other usages. Users won't be
     /// able to create top-level realms with those path segments. Tobira also
     /// has some additional built-in reserved paths (e.g. `/favicon.ico`).
     ///
     /// Example: ["/Shibboleth.sso", "/something-else"]
-    // TODO: this shouldn't be `Option`, but `config(default = ...)` does not
-    // support complex types like this yet.
-    reserved_paths: Option<Vec<String>>,
+    #[config(default = [])]
+    pub reserved_paths: Vec<String>,
 }
 
 const INTERNAL_RESERVED_PATHS: &[&str] = &["favicon.ico", "robots.txt", ".well-known"];
 
 impl GeneralConfig {
-    pub(crate) fn footer_links(&self) -> &[FooterLink] {
-        self.footer_links.as_deref().unwrap_or(&[FooterLink::About, FooterLink::GraphiQL])
-    }
-
-    pub(crate) fn metadata(&self) -> &MetadataLabels {
-        static DEFAULT: Lazy<MetadataLabels> = Lazy::new(|| HashMap::from([
-            ("dcterms".to_string(), HashMap::from([
-                ("license".to_string(), MetadataLabel::License),
-                ("source".to_string(), MetadataLabel::Source),
-            ]))
-        ]));
-
-        self.metadata.as_ref().unwrap_or(&DEFAULT)
-    }
-
     /// Returns an iterator over all reserved top-level paths without leading slash.
     pub(crate) fn reserved_paths(&self) -> impl Iterator<Item = &str> {
-        self.reserved_paths.as_deref()
-            .unwrap_or(&[])
+        self.reserved_paths
             .iter()
             .map(|s| s.strip_prefix("/").unwrap_or(s))
             .chain(INTERNAL_RESERVED_PATHS.iter().copied())
@@ -120,7 +104,7 @@ macro_rules! make_fixed_string_deserializer {
             {
                 use serde::de::{Deserialize, Error};
 
-                match <&str>::deserialize(deserializer)? {
+                match String::deserialize(deserializer)?.as_str() {
                     $s => Ok(()),
                     _ => Err(D::Error::custom(concat!("incorrect value, expected '", $s, "'"))),
                 }
