82757a0a0c3426f48fcc84d42a3326d9d8ec324d
Ole Wieners <olewieners@yahoo.com>
2023-06-20 15:50:45 +0200

    Adjust playwright configuration and CI

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,35 +1,20 @@
 import { defineConfig, devices } from "@playwright/test";
 
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
     testDir: "./tests",
-    workers: 1,
+    workers: process.env.CI ? 1 : undefined,
     retries: 1,
-    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: "html",
-    expect: { timeout: 10 * 1000 },
-    /**
-     * Shared settings for all the projects below.
-     * See https://playwright.dev/docs/api/class-testoptions.
-     */
-    use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: process.env.CI ? "http://localhost:3090" : "http://localhost:8030",
-        headless: true,
-        screenshot: "only-on-failure",
-        locale: "en",
+    expect: { timeout: 20 * 1000 },
 
-        /**
-         * Collect trace when retrying the failed test.
-         * See https://playwright.dev/docs/trace-viewer
-         */
+    use: {
+        baseURL: "http://localhost:3090",
+        headless: true,
+        locale: "en",
         trace: "retain-on-failure",
     },
 
-    /* Configure projects for major browsers */
     projects: [
         {
             name: "chromium",
@@ -48,11 +33,9 @@ export default defineConfig({
         },
     ],
 
-    /* Run your local dev server before starting the tests */
     webServer: {
         command: "cargo run --manifest-path=../backend/Cargo.toml -- serve",
         url: "http://localhost:3090",
-        timeout: 120 * 1000,
         reuseExistingServer: true,
     },
 });
