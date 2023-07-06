d3628a2c95fe9161e14fd0049a703eb2132ab4da
Ole Wieners <olewieners@yahoo.com>
2023-07-06 17:19:42 +0200

    Add "My page" and "Record video" to management navigation
    This is done to increase coherence between user menu and navigation.

--- frontend/src/routes/manage/index.tsx
+++ frontend/src/routes/manage/index.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 import { useTranslation } from "react-i18next";
-import { FiFilm, FiUpload, FiVideo } from "react-icons/fi";
+import { FiExternalLink, FiFilm, FiUpload, FiVideo } from "react-icons/fi";
 import { HiOutlineFire, HiOutlineTemplate } from "react-icons/hi";
 import { graphql } from "react-relay";
 import { useColorScheme } from "@opencast/appkit";
@@ -9,7 +9,7 @@ import { RootLoader } from "../../layout/Root";
 import { makeRoute } from "../../rauta";
 import { loadQuery } from "../../relay";
 import { Link } from "../../router";
-import { LinkList, LinkWithIcon, darkModeBoxShadow } from "../../ui";
+import { LinkList, LinkWithIcon, darkModeBoxShadow, linkWithIconStyle } from "../../ui";
 import { NotAuthorized } from "../../ui/error";
 import { isRealUser, useUser } from "../../User";
 import { Breadcrumbs } from "../../ui/Breadcrumbs";
@@ -126,28 +126,34 @@ const Manage: React.FC = () => {
                 {t("manage.dashboard.studio-tile-body")}
             </ExternalLink>}
         </div>
-        {/* <div css={{ maxWidth: "80ch", fontSize: 14, h2: { marginBottom: 8, fontSize: 18 } }}>
+        <div css={{ maxWidth: "80ch", fontSize: 14, h2: { marginBottom: 8, fontSize: 18 } }}>
             <h2>{t("manage.dashboard.manage-pages-tile-title")}</h2>
             {t("manage.dashboard.manage-pages-tile-body")}
-        </div> */}
+        </div>
     </>;
 };
 
 
 type ManageNavProps = {
-    active?: "/~manage" | "/~manage/videos" | "/~manage/upload";
+    active?: "/~manage" | "/~manage/videos" | "/~manage/upload" | `/@${string}`;
 };
 
 export const ManageNav: React.FC<ManageNavProps> = ({ active }) => {
     const { t } = useTranslation();
     const user = useUser();
     const menu = useMenu();
+    const isDark = useColorScheme().scheme === "dark";
 
     /* eslint-disable react/jsx-key */
     const entries: [NonNullable<ManageNavProps["active"]>, string, ReactElement][] = [
         ["/~manage", t("manage.nav.dashboard"), <HiOutlineTemplate />],
         ["/~manage/videos", t("manage.nav.my-videos"), <FiFilm />],
     ];
+    if (isRealUser(user) && user.canCreateUserRealm) {
+        entries.splice(
+            1, 0, [`/@${user.username}`, t("realm.user-realm.my-page"), <HiOutlineFire />]
+        );
+    }
     if (isRealUser(user) && user.canUpload) {
         entries.push(["/~manage/upload", t("upload.title"), <FiUpload />]);
     }
@@ -165,6 +171,29 @@ export const ManageNav: React.FC<ManageNavProps> = ({ active }) => {
             {label}
         </LinkWithIcon>
     ));
+
+    if (isRealUser(user) && user.canUseStudio) {
+        items.push(
+            <ExternalLink
+                service="STUDIO"
+                params={{ "return.target": new URL(document.location.href) }}
+                fallback="link"
+                css={{
+                    backgroundColor: "inherit",
+                    border: "none",
+                    color: COLORS.primary0,
+                    cursor: "pointer",
+                    width: "100%",
+                    ...linkWithIconStyle(isDark, "left"),
+                    ":hover, :focus-visible": { color: isDark ? COLORS.primary2 : COLORS.primary1 },
+                }}
+            >
+                <FiVideo />
+                {t("manage.dashboard.studio-tile-title")}
+                <FiExternalLink size={18} css={{ marginLeft: 4 }}/>
+            </ExternalLink>
+        );
+    }
 
     return <LinkList items={items} />;
 };
