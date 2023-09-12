67d939aa3b5984bf1ff9b9e80a5ab57ef128a842
Ole Wieners <olewieners@yahoo.com>
2023-09-12 10:07:54 +0200

    Show link by default on all video blocks

--- backend/src/db/migrations/23-video-block-show-link.sql
+++ backend/src/db/migrations/23-video-block-show-link.sql
@@ -1,10 +1,10 @@
 -- Adds a single `show_link` field to blocks, used for video blocks to also show
 -- a link to the corresponding video page. We also drop and re-add the appropriate constraint.
 alter table blocks
-    add column show_link boolean default false,
+    add column show_link boolean default true,
     drop constraint video_block_has_fields,
     add constraint video_block_has_fields check (type <> 'video' or (
         show_title is not null and
         show_link is not null
     ));
-    
+
