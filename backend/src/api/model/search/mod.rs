67f6e86262067edad50b136b41e55bd3c40267ef
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2023-11-15 13:11:26 +0100

    Use Meili's new ranking score to improve search result merging
    This finally gets rid of our hacky previous solution. It's still not
    perfect as in theory, comparing scores between indices with different
    searchable attributes (and stuff) is not well defined. In practice it's
    fine right now. But we should keep an eye on that and fix it at some
    point.

--- backend/src/api/model/search/mod.rs
+++ backend/src/api/model/search/mod.rs
@@ -141,6 +141,7 @@ pub(crate) async fn perform(
     event_query.with_limit(15);
     event_query.with_show_matches_position(true);
     event_query.with_filter(&filter);
+    event_query.with_show_ranking_score(true);
 
 
 
@@ -151,6 +152,7 @@ pub(crate) async fn perform(
         query.with_limit(10);
         query.with_filter("is_user_realm = false");
         query.with_show_matches_position(true);
+        query.with_show_ranking_score(true);
         query
     };
 
@@ -162,97 +164,20 @@ pub(crate) async fn perform(
     );
     let (event_results, realm_results) = handle_search_result!(res, SearchOutcome);
 
-    // Unfortunately, since Meili does not support multi-index search yet, and
-    // since it does not provide any relevance score, we have to merge the
-    // results ourselves. We derive a relevancy score by checking the matches.
-    // This is absolutely not perfect, but it's a semi-useful, better than
-    // random, deterministic way of ordering the results. Multi-index search in
-    // Meili is planned, so hopefully we can get rid of this sometime soon.
-    fn calc_relevancy<T, I, F, G>(
-        results: I,
-        field_weight: F,
-        should_boost: G,
-    ) -> impl Iterator<Item = (NodeValue, f64)>
-    where
-        NodeValue: From<T>,
-        I: IntoIterator<Item = meilisearch_sdk::search::SearchResult<T>>,
-        F: Fn(&str) -> f64,
-        G: Fn(&T) -> bool,
-    {
-        results.into_iter()
-            .map(move |hit| {
-                let mut relevancy = hit.matches_position
-                    .expect("search result has no matches info")
-                    .into_iter()
-                    .map(|(field, matches)| {
-                        // We weigh matches depending on what field was matched.
-                        let weight = field_weight(&field);
+    // Merge results according to Meilis score.
+    //
+    // TODO: Comparing scores of differen indices is not well defined right now.
+    // We can either use score details or adding dummy searchable fields to the
+    // realm index. See this discussion for more info:
+    // https://github.com/orgs/meilisearch/discussions/489#discussioncomment-6160361
+    let events = event_results.hits.into_iter()
+        .map(|result| (NodeValue::from(result.result), result.ranking_score));
+    let realms = realm_results.hits.into_iter()
+        .map(|result| (NodeValue::from(result.result), result.ranking_score));
+    let mut merged = realms.chain(events).collect::<Vec<_>>();
+    merged.sort_unstable_by(|(_, score0), (_, score1)| score1.unwrap().total_cmp(&score0.unwrap()));
 
-                        // We rank the "quality" of the matches like this. Each
-                        // match in itself is worth 2, but the longer the match,
-                        // the better it is. Here we could definitely use a more
-                        // sophisticated algorithm, also looking at the search
-                        // query. But this should be fine for now.
-                        let match_quality: usize = matches.into_iter().map(|m| m.length + 2).sum();
-
-                        weight * match_quality as f64
-                    })
-                    .sum::<f64>();
-
-                if should_boost(&hit.result) {
-                    relevancy += 200.0;
-                }
-
-                (NodeValue::from(hit.result), relevancy)
-            })
-    }
-
-    // Attach a relevancy score to each result, to be able to sort afterwards.
-    let events = calc_relevancy(event_results.hits, |field| {
-        match field {
-            "title" => 10.0,
-            "creators" => 3.0,
-            "description" => 2.0,
-            "series_title" => 1.0,
-            _ => 0.0,
-        }
-    }, |event| event.title.to_lowercase() == user_query.to_lowercase());
-    let realms = calc_relevancy(realm_results.hits, |field| {
-        match field {
-            "name" => 10.0,
-            _ => 0.0,
-        }
-    }, |realm| realm.name.as_ref()
-        .map_or(false, |name| name.to_lowercase() == user_query.to_lowercase()));
-
-
-    // Merge and sort the results. We could simply sort by our own relevancy,
-    // but that is definitely worse than the relevance sorting by Meili. So we
-    // only want to use it to merge both lists together. The order of events
-    // and realms is not changed. The multiplier help in breaking up larger
-    // blocks of one kind.
-    let mut items = Vec::new();
-    let mut events = events.peekable();
-    let mut realms = realms.peekable();
-    let mut event_multiplier = 1.0;
-    let mut realm_multiplier = 1.0;
-    loop {
-        match (events.peek(), realms.peek()) {
-            (None, None) => break,
-            (Some(_), None) => items.push(events.next().unwrap().0),
-            (None, Some(_)) => items.push(realms.next().unwrap().0),
-            (Some(event), Some(realm)) => {
-                if event.1 * event_multiplier >= realm.1 * realm_multiplier {
-                    items.push(events.next().unwrap().0);
-                    event_multiplier *= 0.99;
-                } else {
-                    items.push(realms.next().unwrap().0);
-                    realm_multiplier *= 0.99;
-                }
-            }
-        }
-    }
-
+    let items = merged.into_iter().map(|(node, _)| node).collect();
     Ok(SearchOutcome::Results(SearchResults { items }))
 }
 
