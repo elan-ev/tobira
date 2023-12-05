2637f0fb05f4725ed9fc8e5cd099be7a09d9d208
Ole Wieners <olewieners@yahoo.com>
2023-12-05 18:48:46 +0100

    Fix search ui test

--- frontend/tests/search.spec.ts
+++ frontend/tests/search.spec.ts
@@ -6,6 +6,7 @@ test("Search", async ({ page }) => {
     await navigateTo("/", page);
     await page.waitForSelector("nav");
     const searchField = page.getByPlaceholder("Search");
+    const query = "video";
 
     await test.step("Should be focusable by keyboard shortcut", async () => {
         await page.keyboard.press("s");
@@ -13,18 +14,19 @@ test("Search", async ({ page }) => {
     });
 
     await test.step("Should allow search queries to be executed", async () => {
-        const url = "~search?q=Video";
-        await searchField.fill("Video");
+        const url = `~search?q=${query}`;
+        await searchField.fill(query);
 
         await expect(page).toHaveURL(url);
     });
 
     await test.step("Should show search results", async () => {
+        expect(page.getByText("Search results")).toBeVisible();
         const results = page
-            .locator("div")
-            .filter({ has: page.getByText("Search results for “Video”") })
-            .nth(4);
-        await expect(results).toBeVisible();
-        expect(await results.getByRole("link").count()).toBeGreaterThan(0);
+            .locator("li")
+            .filter({ hasText: "video" })
+            .locator("a");
+        await results.nth(1).waitFor();
+        expect(await results.count()).toBeGreaterThan(0);
     });
 });
