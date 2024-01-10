ab74df5396f290eb9e31eb824b056c6dd2ceedf0
Gregor Eichelberger <gregor.eichelberger@tuwien.ac.at>
2024-01-10 09:54:12 +0100

    Fix test cases
    Signed-off-by: Gregor Eichelberger <gregor.eichelberger@tuwien.ac.at>

--- backend/src/db/tests/util.rs
+++ backend/src/db/tests/util.rs
@@ -136,8 +136,8 @@ impl TestDb {
 
     pub(super) async fn add_series_block(&self, realm: Key, series: Key, index: u8) -> Result<Key> {
         let row = self.query_one(
-            "insert into blocks (realm, index, type, series, show_title, videolist_order)
-                values ($1, $2, 'series', $3, true, 'new_to_old')
+            "insert into blocks (realm, index, type, series, show_title, videolist_order, videolist_view)
+                values ($1, $2, 'series', $3, true, 'new_to_old', 'gallery')
                 returning id",
             &[&realm, &(index as i16), &series],
         ).await?;
