1a8c778afd922689fa929b6f967caac29831c85a
Gregor Eichelberger <gregor.eichelberger@tuwien.ac.at>
2024-01-10 10:23:23 +0100

    Fix migration script
    Fixing the migration script for existing databases by splitting the
    `alter table` statement into two parts. The first statement adds the new
    `videolist_view` column and sets the default value for existing rows. The
    second statement removes the column default and updates the constraints.
    
    Signed-off-by: Gregor Eichelberger <gregor.eichelberger@tuwien.ac.at>

--- backend/src/db/migrations/29-extend-series-block.sql
+++ backend/src/db/migrations/29-extend-series-block.sql
@@ -1,8 +1,13 @@
 -- Adds a single `videolist_view` field to blocks, used for series to define the view
 create type video_list_view as enum ('slider', 'gallery', 'list');
 
+-- Add new `videolist_view` column with default value to initialize existing series blocks
 alter table blocks
-    add column videolist_view video_list_view,
+    add column videolist_view video_list_view default 'gallery';
+
+-- Drop column default and add constraints to ensure that series blocks have all required fields
+alter table blocks
+    alter column videolist_view drop default,
     drop constraint series_block_has_fields,
     add constraint series_block_has_fields check (type <> 'series' or (
         videolist_order is not null and
