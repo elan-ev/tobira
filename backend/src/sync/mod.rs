cbb9fc7f052d711aefab49dd9090538b1c4cee32
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2024-06-21 11:59:26 +0200

    Run `sync texts fetch --daemon` as part of `worker`

--- backend/src/sync/mod.rs
+++ backend/src/sync/mod.rs
@@ -9,9 +9,9 @@ use crate::{config::Config, db::DbConnection, prelude::*};
 pub(crate) mod cmd;
 pub(crate) mod harvest;
 pub(crate) mod stats;
+pub(crate) mod text;
 mod client;
 mod status;
-mod text;
 
 pub(crate) use self::client::OcClient;
 
