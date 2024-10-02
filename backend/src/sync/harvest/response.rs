a9b096b1e61d9d505bf61391dfd9815c1c457b2e
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2024-10-02 14:02:56 +0200

    Add harvest response deserialization tests
    This makes sure that we can keep our promise of being able to
    deserialize old responses from the Harvest API, as specified by
    MIN_REQUIRED_API_VERSION.
    
    This test currently fails, which is the reason I am adding this test
    now: because we screwed up. The following commits will fix this.

--- backend/src/sync/harvest/response.rs
+++ backend/src/sync/harvest/response.rs
@@ -6,6 +6,7 @@ use crate::db::types::{CustomActions, EventCaption, EventTrack, EventSegment, Ex
 
 /// What the harvesting API returns.
 #[derive(Debug, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
 #[serde(rename_all = "camelCase")]
 pub(crate) struct HarvestResponse {
     #[serde(with = "chrono::serde::ts_milliseconds")]
@@ -15,6 +16,7 @@ pub(crate) struct HarvestResponse {
 }
 
 #[derive(Debug, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
 #[serde(tag = "kind")]
 #[serde(rename_all = "kebab-case")]
 pub(crate) enum HarvestItem {
@@ -114,6 +116,7 @@ impl HarvestItem {
 }
 
 #[derive(Debug, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
 #[serde(rename_all = "camelCase")]
 pub(crate) struct Track {
     uri: String,
@@ -136,6 +139,7 @@ impl Into<EventTrack> for Track {
 }
 
 #[derive(Debug, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
 #[serde(rename_all = "camelCase")]
 pub(crate) struct Caption {
     uri: String,
@@ -152,6 +156,7 @@ impl Into<EventCaption> for Caption {
 }
 
 #[derive(Debug, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
 #[serde(rename_all = "camelCase")]
 pub(crate) struct Segment {
     uri: String,
@@ -167,7 +172,8 @@ impl Into<EventSegment> for Segment {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Default)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
 pub(crate) struct Acl {
     #[serde(default)]
     pub(crate) read: Vec<String>,
@@ -178,10 +184,156 @@ pub(crate) struct Acl {
 }
 
 #[derive(Debug, Deserialize)]
+#[cfg_attr(test, derive(PartialEq, Eq))]
 #[serde(rename_all = "camelCase")]
 pub(crate) struct PlaylistEntry {
     pub id: i64,
     #[serde(rename = "type")]
     pub ty: String,
     pub content_id: String,
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+    use super::*;
+
+
+    fn timestamp(ts: u64) -> DateTime<Utc> {
+        DateTime::from_timestamp(ts as i64 / 1000, (ts % 1000) as u32 * 1000_000).unwrap()
+    }
+
+
+    // Makes sure that Tobira can still deserialize a harvest response by the
+    // `MIN_REQUIRED_API_VERSION`. Otherwise, we would have to bump that
+    // version.
+    const OLDEST_SUPPORTED_RESPONSE: &str = include_str!("v1.0-response.json");
+
+    #[test]
+    fn can_deserialize_oldest_supported_response() {
+        let deser = serde_json::from_str::<HarvestResponse>(OLDEST_SUPPORTED_RESPONSE).unwrap();
+        assert_eq!(deser, HarvestResponse {
+            includes_items_until: timestamp(1727867693398),
+            has_more: false,
+            items: vec![
+                HarvestItem::Series {
+                    id: "4b9c6f57-e4af-43dd-ad6e-fee3644fbef4".into(),
+                    title: "Cats".into(),
+                    description: Some("Several videos of cats".into()),
+                    acl: Acl {
+                        read: vec!["ROLE_ANONYMOUS".into()],
+                        write: vec!["ROLE_ANONYMOUS".into()],
+                        custom_actions: CustomActions::default(),
+                    },
+                    updated: timestamp(1727866771932),
+                    created: None,
+                    metadata: ExtraMetadata::default(),
+                },
+                HarvestItem::Event {
+                    id: "002cff10-e0c2-4f0a-a06d-1e5c8dfef13c".into(),
+                    title: "Video Of A Tabby Cat".into(),
+                    description: None,
+                    acl: Acl {
+                        read: vec!["ROLE_ADMIN".into(), "ROLE_ANONYMOUS".into()],
+                        write: vec!["ROLE_ADMIN".into()],
+                        custom_actions: CustomActions::default(),
+                    },
+                    updated: timestamp(1727866860840),
+                    created: timestamp(1727866740000),
+                    creators: vec!["Pixabay".to_owned()],
+                    metadata: ExtraMetadata {
+                        dcterms: HashMap::from([
+                            ("license".to_owned(), vec!["CC0".to_owned()]),
+                        ]),
+                        ..ExtraMetadata::default()
+                    },
+                    part_of: Some("4b9c6f57-e4af-43dd-ad6e-fee3644fbef4".into()),
+                    thumbnail: Some("http://localhost:8080/static/mh_default_org/engage-player/002cff10\
+                        -e0c2-4f0a-a06d-1e5c8dfef13c/11566598-693b-4f94-9787-5d420be66a3d/cat-bokeh\
+                        -x265-480p_1.000s-player.jpg".into()),
+                    slide_text: None,
+                    duration: 11440,
+                    is_live: false,
+                    start_time: None,
+                    end_time: None,
+                    tracks: vec![
+                        Track {
+                            uri: "http://localhost:8080/static/mh_default_org/engage-player/\
+                                002cff10-e0c2-4f0a-a06d-1e5c8dfef13c/7547400e-d872-4bd1-bd63-\
+                                7af340865ae5/cat-bokeh-x265-480p.mp4".into(),
+                            flavor: "presenter/preview".into(),
+                            mimetype: Some("video/mp4".into()),
+                            resolution: Some([854, 480]),
+                            is_master: None,
+                        }
+                    ],
+                    captions: vec![],
+                    segments: vec![],
+                },
+
+                HarvestItem::SeriesDeleted {
+                    id: "eec06048-703d-40b1-a058-478f8bfc13f4".into(),
+                    updated: timestamp(1727867627087),
+                },
+                HarvestItem::EventDeleted {
+                    id: "ef29ba59-2e8e-4949-acaf-8b8e42bed37e".into(),
+                    updated: timestamp(1727867851377),
+                }
+            ],
+        });
+    }
+
+    // Similar to the one above, but trimming it down to the minimal number of
+    // fields specified.
+    const MINIMAL_RESPONSE: &str = include_str!("minimal-response.json");
+
+    #[test]
+    fn can_deserialize_minimal_response() {
+        let deser = serde_json::from_str::<HarvestResponse>(MINIMAL_RESPONSE).unwrap();
+        assert_eq!(deser, HarvestResponse {
+            includes_items_until: timestamp(1727867693398),
+            has_more: false,
+            items: vec![
+                HarvestItem::Series {
+                    id: "4b9c6f57-e4af-43dd-ad6e-fee3644fbef4".into(),
+                    title: "Cats".into(),
+                    description: None,
+                    acl: Acl::default(),
+                    updated: timestamp(1727866771932),
+                    created: None,
+                    metadata: ExtraMetadata::default(),
+                },
+                HarvestItem::Event {
+                    id: "002cff10-e0c2-4f0a-a06d-1e5c8dfef13c".into(),
+                    title: "Video Of A Tabby Cat".into(),
+                    description: None,
+                    acl: Acl::default(),
+                    updated: timestamp(1727866860840),
+                    created: timestamp(1727866740000),
+                    creators: vec!["Pixabay".to_owned()],
+                    metadata: ExtraMetadata::default(),
+                    part_of: None,
+                    thumbnail: None,
+                    slide_text: None,
+                    duration: 11440,
+                    is_live: false,
+                    start_time: None,
+                    end_time: None,
+                    tracks: vec![],
+                    captions: vec![],
+                    segments: vec![],
+                },
+
+                HarvestItem::SeriesDeleted {
+                    id: "eec06048-703d-40b1-a058-478f8bfc13f4".into(),
+                    updated: timestamp(1727867627087),
+                },
+                HarvestItem::EventDeleted {
+                    id: "ef29ba59-2e8e-4949-acaf-8b8e42bed37e".into(),
+                    updated: timestamp(1727867851377),
+                }
+            ],
+        });
+    }
 }
