6769e0864d2f525a7b421457617f85694e757450
Ole Wieners <olewieners@yahoo.com>
2024-12-09 12:26:25 +0100

    Apply frontend changes for updated logo config
    This will now change the logo based on current language,
    if applicable logos exist.

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -30,7 +30,7 @@ type Config = {
     opencast: OpencastConfig;
     footerLinks: FooterLink[];
     metadataLabels: Record<string, Record<string, MetadataLabel>>;
-    logo: LogoConfig;
+    logos: LogoConfig;
     plyr: PlyrConfig;
     upload: UploadConfig;
     paellaPluginConfig: object;
@@ -62,16 +62,12 @@ type AuthConfig = {
 };
 
 type LogoConfig = {
-    large: SingleLogoConfig;
-    small: SingleLogoConfig | null;
-    largeDark: SingleLogoConfig | null;
-    smallDark: SingleLogoConfig | null;
-};
-
-type SingleLogoConfig = {
+    size: "wide" | "narrow" | "*";
+    mode: "light" | "dark" | "*";
+    lang: string;
     path: string;
     resolution: number[];
-};
+}[];
 
 type PlyrConfig = {
     blankVideo: string;
