dbbc7cc9483e141771fa14d215ea42ed949e0a3b
Ole Wieners <olewieners@yahoo.com>
2025-06-03 16:39:40 +0200

    Disable outside click handler
    (but still allow clicking nav and user box buttons).

--- frontend/src/routes/manage/Realm/Content/Edit/EditMode/index.tsx
+++ frontend/src/routes/manage/Realm/Content/Edit/EditMode/index.tsx
@@ -2,18 +2,17 @@ import React, { useRef, useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { graphql, useFragment } from "react-relay";
 import { useForm, useFormContext, FormProvider } from "react-hook-form";
-import { bug, match } from "@opencast/appkit";
+import { FocusTrap } from "focus-trap-react";
+import { bug, match, Button } from "@opencast/appkit";
 
 import { ConfirmationModal, ConfirmationModalHandle } from "../../../../../../ui/Modal";
-import { Button } from "@opencast/appkit";
-import { currentRef, useOnOutsideClick } from "../../../../../../util";
+import { currentRef } from "../../../../../../util";
 import type { EditModeRealmData$key } from "./__generated__/EditModeRealmData.graphql";
 import type { EditModeFormRealmData$key } from "./__generated__/EditModeFormRealmData.graphql";
 import { EditTitleBlock } from "./Title";
 import { EditTextBlock } from "./Text";
 import { EditSeriesBlock } from "./Series";
 import { EditVideoBlock } from "./Video";
-import FocusTrap from "focus-trap-react";
 import { EditPlaylistBlock } from "./Playlist";
 
 
@@ -107,8 +106,6 @@ export const EditModeForm = <FormData extends object, ApiData extends object>(
         }
     };
 
-    useOnOutsideClick(ref, () => handleOnCancel());
-
     const { id: realm, blocks } = useFragment(graphql`
         fragment EditModeFormRealmData on Realm {
             id
@@ -147,7 +144,7 @@ export const EditModeForm = <FormData extends object, ApiData extends object>(
 
 
     return <FormProvider<FormData> {...form}>
-        <FocusTrap>
+        <FocusTrap focusTrapOptions={{ clickOutsideDeactivates: true }}>
             <form ref={ref} onSubmit={onSubmit}>
                 {children}
                 <EditModeButtons onCancel={handleOnCancel} />
