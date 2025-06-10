ebb7818b27a57e6b830a9de883ed0ed0ca53c2d8
Ole Wieners <olewieners@yahoo.com>
2025-06-10 14:36:32 +0200

    WIP

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -3,30 +3,50 @@ import type { ResourceLanguage } from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+import { loadSavedEdits, TranslationCollection, TranslationRecord } from "../routes/Translations";
 import enTranslations from "./locales/en.yaml";
 import deTranslations from "./locales/de.yaml";
 
-export const languages = {
-    en: { translation: enTranslations as ResourceLanguage },
-    de: { translation: deTranslations as ResourceLanguage },
+
+const unflatten = (flat: TranslationRecord) => {
+    const result = {};
+    Object.entries(flat).forEach(([path, value]) => {
+        const keys = path.split(".");
+        let cursor: TranslationRecord | TranslationCollection = result;
+        keys.forEach((key, idx) => {
+            if (idx === keys.length - 1) {
+                cursor[key] = value;
+            } else {
+                if (!cursor[key] || typeof cursor[key] !== "object") {
+                    cursor[key] = {};
+                }
+                cursor = cursor[key];
+            }
+        });
+    });
+    return result;
 };
 
-// TODO: wait for `init` to complete before rendering?
+const saved = loadSavedEdits();
+
+export const languages: Record<string, { translation: ResourceLanguage }> = {
+    en: { translation: enTranslations as ResourceLanguage },
+    de: { translation: deTranslations as ResourceLanguage },
+    it: { translation: saved.it ? unflatten(saved.it) as ResourceLanguage : {} },
+    fr: { translation: saved.fr ? unflatten(saved.fr) as ResourceLanguage : {} },
+};
+
 void i18n
     .use(initReactI18next)
     .use(LanguageDetector)
     .init({
         resources: languages,
-        fallbackLng: "en",
-        interpolation: {
-            escapeValue: false,
-        },
-        detection: {
-            order: ["localStorage", "navigator"],
-        },
-        react: {
-            transKeepBasicHtmlNodesFor: ["br", "strong", "i", "p", "code"],
-        },
+        interpolation: { escapeValue: false },
+        detection: { order: ["localStorage", "navigator"] },
+        react: { transKeepBasicHtmlNodesFor: ["br", "strong", "i", "p", "code"] },
+        returnEmptyString: false,
+        returnNull: false,
+        parseMissingKeyHandler: key => `⚠️ ${key}`,
     });
 
 export default i18n;
