bd4e0406f167876d992844417e23fe7b77544485
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2025-08-22 13:45:50 +0200

    Use shortcut components from appkit

--- frontend/src/ui/Shortcuts.tsx
+++ frontend/src/ui/Shortcuts.tsx
@@ -8,11 +8,12 @@ import {
     LuArrowDown,
     LuArrowUp,
 } from "react-icons/lu";
-import { match, screenWidthAtMost, useColorScheme } from "@opencast/appkit";
+import {
+    KeyCombinationContainer, match, ShortcutGroupOverview, SingleKeyContainer,
+} from "@opencast/appkit";
 import TabIcon from "@opencast/appkit/dist/icons/tab-key.svg";
 
 import { Modal, ModalHandle } from "./Modal";
-import { COLORS } from "../color";
 import { SKIP_INTERVAL } from "./player/consts";
 import { TranslationKey } from "../i18n";
 
@@ -106,7 +107,6 @@ export const ShortcutsOverview: React.FC<{ modalRef: React.RefObject<ModalHandle
 }) => {
     const { t } = useTranslation();
     const groups: (keyof typeof SHORTCUTS)[] = ["general", "player"];
-
     return (
         <Modal
             className="disable-background-scroll"
@@ -114,6 +114,7 @@ export const ShortcutsOverview: React.FC<{ modalRef: React.RefObject<ModalHandle
             title={t("shortcuts.title")}
             closeOnOutsideClick
             css={{
+                width: "80%",
                 maxWidth: 1000,
                 "& > div": {
                     maxHeight: "80vh",
@@ -121,55 +122,19 @@ export const ShortcutsOverview: React.FC<{ modalRef: React.RefObject<ModalHandle
                 },
             }}
         >
-            {groups.map(group => (
-                <section key={group} css={{
-                    margin: "32px 0",
-                    ":first-of-type": {
-                        marginTop: 16,
-                    },
-                }}>
-                    <h2 css={{ fontSize: 18, marginBottom: 8 }}>
-                        {t(`shortcuts.${group}.title`)}
-                    </h2>
-                    <div css={{ display: "flex", flexWrap: "wrap", gap: 12 }}>
-                        {Object.entries(SHORTCUTS[group]).map((
-                            [name, shortcut]: [string, ShortcutProps],
-                        ) => <div
-                            key={group + name}
-                            css={{
-                                width: "calc(33.33% - 24px / 3)",
-                                [screenWidthAtMost(1000)]: {
-                                    width: "calc(50% - 12px / 2)",
-                                },
-                                [screenWidthAtMost(720)]: {
-                                    width: "100%",
-                                },
-                                backgroundColor: COLORS.neutral10,
-                                borderRadius: 4,
-                                padding: "10px 16px",
-                                display: "inline-flex",
-                                flexDirection: "column",
-                                alignItems: "start",
-                                gap: 8,
-                            }}
-                        >
-                            <div css={{ overflowWrap: "anywhere" }}>
-                                {typeof shortcut.translation === "string"
-                                    ? t(shortcut.translation)
-                                    : t(shortcut.translation.key, shortcut.translation.options)}
-                            </div>
-                            <div css={{ display: "flex", gap: 8, alignItems: "center" }}>
-                                {shortcut.keys.split(";").map((combination: string, i: number) =>
-                                    <React.Fragment key={i}>
-                                        {i > 0 && t("shortcuts.sequence-separator")}
-                                        <ShortcutKeys shortcut={combination.trim()} />
-                                    </React.Fragment>)
-                                }
-                            </div>
-                        </div>)}
-                    </div>
-                </section>
-            ))}
+            {groups.map(group => <ShortcutGroupOverview
+                key={group}
+                alternativeSeparator={t("shortcuts.sequence-separator")}
+                title={t(`shortcuts.${group}.title`)}
+                shortcuts={Object.values(SHORTCUTS[group]).map((shortcut: ShortcutProps) => ({
+                    label: typeof shortcut.translation === "string"
+                        ? t(shortcut.translation)
+                        : t(shortcut.translation.key, shortcut.translation.options),
+                    combinations: shortcut.keys.split(";").map((combination, i) => (
+                        <ShortcutKeys key={i} shortcut={combination.trim()} />
+                    )),
+                }))}
+            />)}
         </Modal>
     );
 };
@@ -178,7 +143,7 @@ export const ShortcutsOverview: React.FC<{ modalRef: React.RefObject<ModalHandle
 const ShortcutKeys: React.FC<{ shortcut: string }> = ({ shortcut }) => {
     const { t } = useTranslation();
 
-    return <div css={{ display: "flex", alignItems: "center", gap: 4, color: COLORS.neutral70 }}>
+    return <KeyCombinationContainer>
         {shortcut.split("+").map((key, i) => {
             const child = match(key, {
                 "escape": () => <>{t("shortcuts.keys.escape")}</>,
@@ -192,47 +157,12 @@ const ShortcutKeys: React.FC<{ shortcut: string }> = ({ shortcut }) => {
                 "shift": () => <>{t("shortcuts.keys.shift")}<LuArrowBigUp /></>,
                 "tab": () => <>{t("shortcuts.keys.tab")}<TabIcon /></>,
             }) ?? <>{key}</>;
-            return (
-                <React.Fragment key={shortcut + key}>
-                    {i !== 0 && "+"}
-                    <SingleKey monofont={key === "l"}>{child}</SingleKey>
-                </React.Fragment>
-            );
+            return <SingleKeyContainer key={i} css={key === "l" ? { fontFamily: "monospace" } : {}}>
+                {child}
+            </SingleKeyContainer>;
         })}
-    </div>;
+    </KeyCombinationContainer>;
 };
-
-
-type SingleKeyProps = React.PropsWithChildren<{
-  /** Whether to use `monospace` font for this one. Basically only useful for lowercase l. */
-  monofont: boolean;
-}>;
-
-const SingleKey: React.FC<SingleKeyProps> = ({ monofont, children }) => {
-    const isLight = useColorScheme().scheme === "light";
-
-    return (
-        <div css={{
-            border: `1px solid ${COLORS.neutral50}`,
-            borderRadius: 4,
-            padding: "2px 6px",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            height: 36,
-            minWidth: 36,
-            fontSize: 16,
-            boxShadow: "0 0 6px var(--shadow-color)",
-            backgroundColor: isLight ? COLORS.neutral05 : COLORS.neutral15,
-            color: isLight ? COLORS.neutral80 : COLORS.neutral90,
-            cursor: "default",
-            ...monofont && { fontFamily: "monospace" },
-        }}>
-            {children}
-        </div>
-    );
-};
-
 
 export const useShortcut = (
     keys: string,
