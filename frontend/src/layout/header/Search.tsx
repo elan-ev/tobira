3d97091e16b002d599f8bd94e186dce0b20cb0ce
Ole Wieners <olewieners@yahoo.com>
2025-08-29 16:30:54 +0200

    Refactor search field component
    To make it reusable.

--- frontend/src/layout/header/Search.tsx
+++ frontend/src/layout/header/Search.tsx
@@ -26,7 +26,7 @@ type SearchFieldProps = {
 export const SearchField: React.FC<SearchFieldProps> = ({ variant }) => {
     const { t } = useTranslation();
     const router = useRouter();
-    const ref = useRef<HTMLInputElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
 
     // If the user is unknown, then we are still in the initial loading phase.
     // We don't want users to input anything into the search field in that
@@ -36,7 +36,7 @@ export const SearchField: React.FC<SearchFieldProps> = ({ variant }) => {
 
     useShortcut(
         SHORTCUTS.general.search.keys,
-        () => ref.current?.focus(),
+        () => inputRef.current?.focus(),
         {
             preventDefault: true,
             // Will cause infinite rerenders if not memoized, hence the `useCallback`.
@@ -49,12 +49,6 @@ export const SearchField: React.FC<SearchFieldProps> = ({ variant }) => {
         },
     );
 
-    const height = 42;
-    const spinnerSize = 24;
-    const paddingSpinner = (height - spinnerSize) / 2;
-    const iconStyle = { position: "absolute", right: paddingSpinner, top: paddingSpinner } as const;
-
-
     const onSearchRoute = isSearchActive();
     const getSearchParam = (searchParameter: string) => {
         const searchParams = new URLSearchParams(document.location.search);
@@ -63,8 +57,6 @@ export const SearchField: React.FC<SearchFieldProps> = ({ variant }) => {
             : undefined;
     };
     const defaultValue = getSearchParam("q");
-    const [inputIsEmpty, setInputIsEmpty] = useState(!defaultValue);
-
 
     const search = (q: string) => {
         if (!(q in SEARCH_TIMINGS)) {
@@ -79,13 +71,62 @@ export const SearchField: React.FC<SearchFieldProps> = ({ variant }) => {
         router.goto(SearchRoute.url({ query: q, ...filters }));
     };
 
+    const clear = () => {
+        const input = currentRef(inputRef);
+        input.value = "";
+        input.focus();
+    };
+
+    return <SearchInput
+        {...{ search, inputRef, clear, defaultValue, variant }}
+        inputProps={{
+            disabled,
+            placeholder: t("search.input-label"),
+            // We only want to autofocus if the user just pressed
+            // the search button in the header (mobile only). This
+            // only happens on non-search routes.
+            autoFocus: variant === "mobile" && !onSearchRoute,
+        }}
+    />;
+};
+
+
+type SearchInputProps = Partial<SearchFieldProps> & {
+    search: (q: string) => void;
+    inputRef: React.RefObject<HTMLInputElement>;
+    clear: () => void;
+    inputProps: React.InputHTMLAttributes<HTMLInputElement>;
+    defaultValue?: string;
+}
+
+export const SearchInput: React.FC<SearchInputProps> = ({
+    search,
+    inputRef,
+    clear,
+    inputProps,
+    defaultValue,
+    variant,
+}) => {
+    const { t } = useTranslation();
+    const [inputIsEmpty, setInputIsEmpty] = useState(!defaultValue);
+
+    const handleClear = () => {
+        clear();
+        setInputIsEmpty(true);
+    };
+
+    const height = 42;
+    const spinnerSize = 24;
+    const paddingSpinner = (height - spinnerSize) / 2;
+
+
     return (
         <div css={{
             position: "relative",
-            margin: "0 8px",
             flex: 1,
+            ...variant && { margin: "0 8px" },
+            ...variant !== "mobile" && { maxWidth: 372 },
             ...variant === "desktop" && {
-                maxWidth: 372,
                 [screenWidthAtMost(NAV_BREAKPOINT)]: {
                     display: "none",
                 },
@@ -100,7 +141,7 @@ export const SearchField: React.FC<SearchFieldProps> = ({ variant }) => {
             }} />
             <form onSubmit={event => {
                 event.preventDefault();
-                search(currentRef(ref).value);
+                search(currentRef(inputRef).value);
 
                 // Hide mobile keyboard on enter. The mobile keyboard hides lots
                 // of results and intuitively, pressing "enter" on it should
@@ -110,22 +151,41 @@ export const SearchField: React.FC<SearchFieldProps> = ({ variant }) => {
                 // keyboard very reliably.
                 const visualHeight = window.visualViewport?.height;
                 if (visualHeight && visualHeight < window.innerHeight) {
-                    ref.current?.blur();
+                    inputRef.current?.blur();
                 }
             }}>
-                <label css={{ display: "flex" }}>
-                    <span css={{ display: "none" }}>{t("search.input-label")}</span>
+                <label css={{
+                    display: "flex",
+                    input: {
+                        flex: 1,
+                        color: COLORS.neutral60,
+                        backgroundColor: COLORS.neutral05,
+                        border: `1px solid ${COLORS.neutral40}`,
+                        borderRadius: 4,
+                        minWidth: 50,
+                        height,
+                        paddingLeft: 42,
+                        paddingRight: 12,
+                        ":hover": {
+                            borderColor: COLORS.neutral25,
+                            outline: `2.5px solid ${COLORS.neutral25}`,
+                            outlineOffset: -1,
+                        },
+                        ":focus-visible": { borderColor: COLORS.focus },
+                        ...focusStyle({ offset: -1 }),
+                        "&::placeholder": {
+                            color: COLORS.neutral60,
+                            opacity: 1,
+                        },
+                    },
+                }}>
+                    <span css={{ display: "none" }}>{inputProps.placeholder}</span>
                     <input
-                        ref={ref}
+                        ref={inputRef}
                         type="text"
-                        disabled={disabled}
-                        placeholder={t("search.input-label")}
                         defaultValue={defaultValue}
                         onChange={e => setInputIsEmpty(e.currentTarget.value.length === 0)}
-                        // We only want to autofocus if the user just pressed
-                        // the search button in the header (mobile only). This
-                        // only happens on non-search routes.
-                        autoFocus={variant === "mobile" && !onSearchRoute}
+                        {...inputProps}
                         css={{
                             flex: 1,
                             color: COLORS.neutral60,
@@ -154,12 +214,7 @@ export const SearchField: React.FC<SearchFieldProps> = ({ variant }) => {
             {!inputIsEmpty && <ProtoButton
                 aria-label={t("search.clear")}
                 // Just clear the search input
-                onClick={() => {
-                    const input = currentRef(ref);
-                    input.value = "";
-                    input.focus();
-                    setInputIsEmpty(true);
-                }}
+                onClick={handleClear}
                 css={{
                     ":hover, :focus": {
                         color: COLORS.neutral90,
@@ -169,9 +224,13 @@ export const SearchField: React.FC<SearchFieldProps> = ({ variant }) => {
                     ...focusStyle({}),
                     borderRadius: 4,
                     color: COLORS.neutral60,
-                    ...iconStyle,
+                    position: "absolute",
+                    right: paddingSpinner,
+                    top: paddingSpinner,
                 }}
-            ><LuX size={spinnerSize} css={{ display: "block" }} /></ProtoButton>}
+            >
+                <LuX size={spinnerSize} css={{ display: "block" }} />
+            </ProtoButton>}
         </div>
     );
 };
