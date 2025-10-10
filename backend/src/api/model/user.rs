b37c7020253ea5b2714ac1865139986ce7eae018
Ole Wieners <olewieners@yahoo.com>
2025-10-10 12:47:30 +0200

    Add "my playlists" route
    This is the table overview analogous to
    "my videos" and "my series". It is missing
    some stuff like thumbnails for now, that will
    be added in a later commit.

--- backend/src/api/model/user.rs
+++ backend/src/api/model/user.rs
@@ -1,8 +1,11 @@
 use crate::{
     api::{
-        Context,
         err::ApiResult,
-        model::event::AuthorizedEvent,
+        model::{
+            event::AuthorizedEvent,
+            playlist::{AuthorizedPlaylist, Playlist, PlaylistsSortOrder},
+        },
+        Context,
     },
     auth::User,
     prelude::*,
@@ -121,5 +124,20 @@ impl User {
         filter: Option<SearchFilter>,
     ) -> ApiResult<Connection<Series>> {
         Series::load_writable_for_user(context, order.into(), offset, limit, filter).await
+    }
+
+    /// Returns all playlists that somehow "belong" to the user, i.e. that appear
+    /// on the "my playlists" page.
+    async fn my_playlists(
+        &self,
+        context: &Context,
+        #[graphql(default)]
+        order: PlaylistsSortOrder,
+        offset: i32,
+        limit: i32,
+        #[graphql(default)]
+        filter: Option<SearchFilter>,
+    ) -> ApiResult<Connection<AuthorizedPlaylist>> {
+        Playlist::load_writable_for_user(context, order.into(), offset, limit, filter).await
     }
 }
---
+++ frontend/src/routes/manage/Playlist/index.tsx
@@ -0,0 +1,133 @@
+import { graphql } from "react-relay";
+import { match } from "@opencast/appkit";
+
+import i18n from "../../../i18n";
+import { ManageNav } from "..";
+import { RootLoader } from "../../../layout/Root";
+import { makeRoute } from "../../../rauta";
+import { loadQuery } from "../../../relay";
+import { NotAuthorized } from "../../../ui/error";
+import {
+    ColumnProps,
+    createQueryParamsParser,
+    DateColumn,
+    ManageItems,
+    TableRow,
+} from "../Shared/Table";
+
+import { keyOfId } from "../../../util";
+import {
+    PlaylistsManageQuery,
+    PlaylistsManageQuery$data,
+    PlaylistsSortColumn,
+} from "./__generated__/PlaylistsManageQuery.graphql";
+
+
+export const PATH = "/~manage/playlists" as const;
+
+export const ManagePlaylistsRoute = makeRoute({
+    url: PATH,
+    match: url => {
+        if (url.pathname !== PATH) {
+            return null;
+        }
+
+        const vars = queryParamsToPlaylistsVars(url.searchParams);
+        const titleFilter = vars.filters?.title ?? null;
+        const queryVars = {
+            ...vars,
+            // Todo: Adjust when more filter options are added
+            filter: titleFilter ? { title: titleFilter } : null,
+        };
+        const queryRef = loadQuery<PlaylistsManageQuery>(query, queryVars);
+
+        return {
+            render: () => <RootLoader
+                {...{ query, queryRef }}
+                noindex
+                nav={() => <ManageNav active={PATH} />}
+                render={data => !data.currentUser
+                    ? <NotAuthorized />
+                    : <ManageItems
+                        vars={vars}
+                        connection={data.currentUser.myPlaylists}
+                        titleKey="manage.playlist.table.title"
+                        additionalColumns={playlistColumns}
+                        RenderRow={PlaylistRow}
+                    />
+                }
+            />,
+            dispose: () => queryRef.dispose(),
+        };
+    },
+});
+
+const query = graphql`
+    query PlaylistsManageQuery(
+        $order: PlaylistsSortOrder!,
+        $offset: Int!,
+        $limit: Int!,
+        $filter: SearchFilter,
+    ) {
+        ...UserData
+        currentUser {
+            myPlaylists(order: $order, offset: $offset, limit: $limit, filter: $filter) {
+                __typename
+                totalCount
+                pageInfo { hasNextPage hasPrevPage }
+                items {
+                    id
+                    title
+                    updated
+                    description
+                    numVideos
+                }
+            }
+        }
+    }
+`;
+
+
+export type PlaylistConnection
+    = NonNullable<PlaylistsManageQuery$data["currentUser"]>["myPlaylists"];
+export type Playlists = PlaylistConnection["items"];
+export type SinglePlaylist = Playlists[number];
+
+const playlistColumns: ColumnProps<SinglePlaylist>[] = [
+    {
+        key: "EVENT_COUNT",
+        label: "video.plural",
+        headerWidth: 112,
+        column: ({ item }) => <td css={{ fontSize: 14 }}>
+            {i18n.t("manage.video-list.no-of-videos", { count: item.numVideos })}
+        </td>,
+    },
+    {
+        key: "UPDATED",
+        label: "manage.table.columns.updated",
+        column: ({ item }) => <DateColumn date={item.updated} />,
+    },
+];
+
+
+const PlaylistRow: React.FC<{ item: SinglePlaylist }> = ({ item }) => <TableRow
+    itemType="series" // Todo: change to "playlist"
+    item={{ ...item, state: "READY" }}
+    thumbnail={_status => <></>} // Todo: add thumbnails
+    link={`${PATH}/${keyOfId(item.id)}`}
+    customColumns={playlistColumns.map(col => <col.column key={col.key} item={item} />)}
+/>;
+
+
+
+const parsePlaylistColumn = (sortBy: string | null): PlaylistsSortColumn =>
+    sortBy !== null
+        ? match(sortBy, {
+            "title": () => "TITLE" as const,
+            "updated": () => "UPDATED" as const,
+            "event_count": () => "EVENT_COUNT" as const,
+        }) ?? "UPDATED"
+        : "UPDATED";
+
+const queryParamsToPlaylistsVars
+    = createQueryParamsParser<PlaylistsSortColumn>(parsePlaylistColumn);
