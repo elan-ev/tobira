5a2e93d618800da7d271a59f344a177ec04f147f
Ole Wieners <olewieners@yahoo.com>
2025-11-14 15:21:30 +0100

    Adjust config docs
    This adds some warning to the config docs to urge people
    to still use `player+preview` as subtype flavor for their
    final publications, as it was brought to my attention that this
    subtype value is hardcoded in multiple places in Opencast and
    thus is heavily relied upon.
    Using other flavors for that just isn't supported.
    That is only relevant when the *attachment* subtype is changed
    to use something different from the default value.

--- backend/src/config/upload.rs
+++ backend/src/config/upload.rs
@@ -11,8 +11,12 @@ pub(crate) struct UploadConfig {
     pub workflow: Option<String>,
 
     /// Subtype of thumbnail image. Has to match the subtype used in the related
-    /// Opencast workflows and should be identical to what is used for the editor.
+    /// Opencast workflows and should be identical to what is set for the editor.
     /// See https://github.com/opencast/opencast/blob/a68c337f11499a0a939a5a96bffa3898c1cd032c/etc/org.opencastproject.editor.EditorServiceImpl.cfg#L58
+    ///
+    /// Please note that `player+preview` should still be used for the final publication
+    /// as there are multiple operations in Opencast that rely on that hardcoded subtype,
+    /// meaning it cannot be overwritten by any configuration in these places.
     #[config(default = "player+preview")]
     pub thumbnail_subtype: String,
 
@@ -20,7 +24,7 @@ pub(crate) struct UploadConfig {
     /// the operation that prepares the thumbnail for publication
     /// in the default `partial-publish` workflow.
     /// This needs to match the property in Opencast and should also be identical
-    /// to what is used for the editor.
+    /// to what is set for the editor when you don't use custom workflows for publication.
     /// See https://github.com/opencast/opencast/blob/a68c337f11499a0a939a5a96bffa3898c1cd032c/etc/org.opencastproject.editor.EditorServiceImpl.cfg#L62
     #[config(default = "thumbnail_edited")]
     pub thumbnail_workflow_property: String,
