5c38a66d77c516f3729858aaa9a815901f86d7cc
Ole Wieners <olewieners@yahoo.com>
2025-12-04 17:43:58 +0100

    Change layout of buttons in series blocks
    Insert 'css is awesome' meme.

--- frontend/src/ui/ShareButton.tsx
+++ frontend/src/ui/ShareButton.tsx
@@ -22,9 +22,12 @@ export type ShareButtonProps = {
     onOpen?: () => void;
     height: number;
     className?: string;
+    hideLabel?: boolean;
 };
 
-export const ShareButton: React.FC<ShareButtonProps> = ({ tabs, onOpen, height, className }) => {
+export const ShareButton: React.FC<ShareButtonProps> = ({
+    tabs, onOpen, height, className, hideLabel = false,
+}) => {
     const { t } = useTranslation();
     const [activeTab, setActiveTab] = useState<string | null>(null);
     const isDark = useColorScheme().scheme === "dark";
@@ -107,14 +110,14 @@ export const ShareButton: React.FC<ShareButtonProps> = ({ tabs, onOpen, height, 
         >
             {/* Share Button */}
             <FloatingTrigger>
-                <Button {...{ className }} onClick={() => {
+                <Button aria-label={t("series.share")} {...{ className }} onClick={() => {
                     setActiveTab(state => state === null ? Object.keys(tabs)[0] : null);
                     if (activeTab == null) {
                         onOpen?.();
                     }
                 }}>
                     <LuShare2 size={16} />
-                    {t("general.action.share")}
+                    {!hideLabel && t("general.action.share")}
                 </Button>
             </FloatingTrigger>
 
