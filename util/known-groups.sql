50ef3edcee84133125905ab66deef893f0c3af96
Ole Wieners <olewieners@yahoo.com>
2026-01-13 17:55:57 +0100

    Fix `known-groups.sql` script
    This was still using the `en` label instead of `default`.

--- util/known-groups.sql
+++ util/known-groups.sql
@@ -2,28 +2,28 @@
 -- ones taken from the realm tree, representing individual courses.
 
 insert into known_groups (role, label, implies, large)
-values 
-    ('ROLE_STUDENTS', hstore(array['en', 'Students', 'de', 'Studierende']), '{}', true),
-    ('ROLE_STAFF', hstore(array['en', 'Staff', 'de', 'Angestellte']), '{}', true),
-    ('ROLE_LECTURER', hstore(array['en', 'Lecturers', 'de', 'Vortragende']), array['ROLE_STAFF'], true);
+values
+    ('ROLE_STUDENTS', hstore(array['default', 'Students', 'de', 'Studierende']), '{}', true),
+    ('ROLE_STAFF', hstore(array['default', 'Staff', 'de', 'Angestellte']), '{}', true),
+    ('ROLE_LECTURER', hstore(array['default', 'Lecturers', 'de', 'Vortragende']), array['ROLE_STAFF'], true);
 
 insert into known_groups (role, label, implies, large)
-select 
+select
     'ROLE_COURSE_' || path_segment || '_' || kind,
-    hstore(array['en', 'de'], array[
+    hstore(array['default', 'de'], array[
         label_en || ' of „' || name || '“',
         label_de || ' von „' || name || '“'
     ]),
-    case 
+    case
         when kind = 'STUDENTS' then array[]::text[]
         when kind = 'ASSISTANTS' then array['ROLE_COURSE_' || path_segment || '_STUDENTS']
         when kind = 'INSTRUCTORS' then array['ROLE_COURSE_' || path_segment || '_ASSISTANTS']
     end,
     false
-from realms, 
-    (values 
-        ('STUDENTS', 'Studierende', 'Students'), 
-        ('ASSISTANTS', 'Assistierende', 'Assistants'), 
+from realms,
+    (values
+        ('STUDENTS', 'Studierende', 'Students'),
+        ('ASSISTANTS', 'Assistierende', 'Assistants'),
         ('INSTRUCTORS', 'Lehrende', 'Instructors')
     ) as tmp (kind, label_de, label_en)
 where full_path similar to '/lectures/[^/]+/2020/(autumn|spring)/%'
