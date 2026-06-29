51dbd9f7f5ed29b7a4bb9a5ea9ce6e44a1df3f81
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2026-06-29 18:01:39 +0200

    Manually define `VideoListLayout` instead of using API type
    This is nicer for components we write. API types should not used deep
    inside our components, but just "close to the API request" basically.
    I now also fallback to GALLERY when encountering unknown values.

--- frontend/src/ui/Blocks/VideoList.tsx
+++ frontend/src/ui/Blocks/VideoList.tsx
@@ -22,7 +22,6 @@ import {
 } from "react-icons/lu";
 import { graphql, readInlineData } from "react-relay";
 
-import { VideoListLayout } from "./__generated__/SeriesBlockData.graphql";
 import {
     VideoListEventData$data,
     VideoListEventData$key,
@@ -539,6 +538,7 @@ const VideoListManageButton: React.FC<VideoListManageButtonProps> = ({ link, kin
 
 export const LIST_ORDERS = ["ORIGINAL", "AZ", "ZA", "NEW_TO_OLD", "OLD_TO_NEW"] as const;
 export type Order = typeof LIST_ORDERS[number];
+export type VideoListLayout = "GALLERY" | "LIST" | "SLIDER";
 
 type LayoutOrderContext = {
     layoutState: VideoListLayout;
@@ -582,7 +582,6 @@ const LayoutMenu: React.FC = () => {
         SLIDER: () => <LuColumns2 />,
         GALLERY: () => <LuLayoutGrid />,
         LIST: () => <LuList />,
-        "%future added value": () => unreachable(),
     });
 
     const triggerContent = (
@@ -798,10 +797,6 @@ const Items: React.FC<ItemsProps> = ({
 
     if (layoutState === "SLIDER") {
         return <SliderView {...{ itemLink, items }} />;
-    }
-
-    if (layoutState === "%future added value") {
-        return unreachable();
     }
 
     const content = match(layoutState, {
